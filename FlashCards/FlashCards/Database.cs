5bb08763cda32a85a79af0a15acca8319f967290
benbarnett02 <46703294+benbarnett02@users.noreply.github.com>
2022-12-18 12:15:15 +1100

    Added database class & updated csproj

---
+++ FlashCards/FlashCards/Database.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards
+{
+    public class Database
+    {
+        public class FlashCard
+        {
+            // The DTO?
+            public string Id { get; set; }
+            public string Challenge { get; set; }
+            public string Answer { get; set; }
+            public string Blah { get; set; }
+        }
+
+        public static FlashCard GetFlashCard()
+        {
+            // The method to set the DTO?
+            FlashCard thisFlashCard = new()
+            {
+                Id = "1",
+                Challenge = "The question",
+                Answer = "The Answer",
+                Blah = "Another thing"
+            };
+
+            return thisFlashCard;
+        }
+
+    }
+
+    
+
+
+}
