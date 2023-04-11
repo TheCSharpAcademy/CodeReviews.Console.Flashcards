3f78298f742d7b95efa91132b3c5767160042b10
Hugo Delautre <97909616+Gautatyr@users.noreply.github.com>
2023-04-11 06:46:14 +0200

    Hopefully fixes the folder problem

---
+++ Gautatyr.FlashCards/FlashCards/Models/FlashCard.cs
@@ -0,0 +1,22 @@
+﻿namespace FlashCards.Models;
+
+public class Card
+{
+    public int Id { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    public int StackId { get; set; }
+}
+
+public class CardDTO
+{
+    public int Id { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+}
+
+public class CardNoId
+{
+    public string Question { get; set; }
+    public string Answer { get; set; }
+}
---
+++ Gautatyr.FlashCards/FlashCards/Models/Stack.cs
@@ -0,0 +1,19 @@
+﻿namespace FlashCards.Models;
+
+public class Stack
+{
+    public int Id { get; set; }
+    public string Theme { get; set; }
+}
+
+public class StackCardsDTO
+{
+    public string Theme { get; set; }
+    public List<CardDTO> CardsDTO { get; set; }
+}
+
+public class StackCardsWithId
+{
+    public string Theme { get; set; }
+    public int Id { get; set; }
+}
