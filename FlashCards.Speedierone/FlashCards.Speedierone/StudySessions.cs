6d29794081c808b1c446ea68d9b0b45d36dbe8a0
simon davies <daviessimon21@gmail.com>
2023-05-14 18:49:03 +0100

    Complete.

---
+++ FlashCards.Speedierone/FlashCards.Speedierone/StudySessions.cs
@@ -0,0 +1,13 @@
+﻿namespace FlashCards;
+
+internal class StudySessions
+{
+    public int ID { get; set; }
+    public string Subject { get; set;}
+    public string FlashSubject { get; set;}
+    public string FrontCard { get; set;}
+    public string BackCard { get; set;}
+    public DateTime Date { get; set;}
+    public int GameScore { get; set;}
+    public int GameAmount { get; set;}
+}
