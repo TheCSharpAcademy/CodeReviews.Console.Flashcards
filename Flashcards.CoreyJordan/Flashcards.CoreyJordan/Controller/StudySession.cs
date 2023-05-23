0ae29124c8a975f73dba5e850af4628d5b7e6bcb
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2023-05-23 19:34:18 -0400

    Comment plan for Study Sessions

--- Flashcards.CoreyJordan/Flashcards.CoreyJordan/Controller/StudySession.cs
+++ Flashcards.CoreyJordan/Flashcards.CoreyJordan/Controller/StudySession.cs
@@ -3,6 +3,22 @@ internal class StudySession : Controller
 {
     internal void StartNew()
     {
+        // Get user name
+        // Choose pack
+        // Create list of correct answers
+        // Create list of incorrect answers
+
+        // Move all cards to incorrect
+
+        // Show card
+        // Get answer
+        // Show back
+        // Report result
+        // If correct, add to correct list and remove from incorrect
+        // Loop until incorrect is empty or user quits
+
+        // Report results
+        // Store results in db
         throw new NotImplementedException();
     }
 }
