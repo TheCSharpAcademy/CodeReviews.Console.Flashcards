9d1d88e61e4e33f09f587ef9d9560599d6d52757
furiax <carlmalfliet@proximus.be>
2023-06-30 11:04:01 +0200

    Setup connection to sql server and testing it

---
+++ Furiax.FlashCards/FlashCards/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+
+string connectionString = ConfigurationManager.ConnectionStrings["Flashcards"].ConnectionString;
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+	try
+	{
+		connection.Open();
+		if (connection.State == System.Data.ConnectionState.Open)
+		{
+            Console.WriteLine("connection succesfull");
+        }
+		else
+		{ Console.WriteLine("connection failed"); }
+	}
+	catch (Exception ex)
+	{
+        Console.WriteLine("An error occured: " + ex);
+    }
+}
