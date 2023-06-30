4549e7d89faed2553337cc8bc623f58f958c99b3
furiax <carlmalfliet@proximus.be>
2023-06-30 13:36:56 +0200

    created models

---
+++ Furiax.FlashCards/FlashCards/UserInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards
+{
+	internal class UserInput
+	{
+		internal static void ShowMenu()
+		{
+            Console.WriteLine("Flashcards");
+            Console.WriteLine("----------");
+			Console.WriteLine("1. Manage Stacks");
+            Console.WriteLine("2. Manage Flashcards");
+            Console.WriteLine("3. Study");
+            Console.WriteLine("4. View Study Data");
+            Console.WriteLine("0. Exit");
+        }
+	}
+}
