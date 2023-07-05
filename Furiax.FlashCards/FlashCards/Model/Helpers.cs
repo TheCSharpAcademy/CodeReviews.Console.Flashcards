76a9b4d15a317e925873d4360fc950d4fc3ad011
furiax <carlmalfliet@proximus.be>
2023-07-05 21:10:28 +0200

    Working on the menu layout

---
+++ Furiax.FlashCards/FlashCards/Model/Helpers.cs
@@ -0,0 +1,41 @@
+﻿namespace FlashCards.Model
+{
+	internal class Helpers
+	{
+		internal static void MainMenu()
+		{
+			Console.WriteLine("Flashcards");
+			Console.WriteLine("----------");
+			Console.WriteLine("1. Manage Stacks");
+			Console.WriteLine("2. Manage Flashcards");
+			Console.WriteLine("3. Study");
+			Console.WriteLine("4. View Study Data");
+			Console.WriteLine("0. Exit");
+			Console.WriteLine("--------------------");
+			Console.WriteLine("Please make a choice by entering the corresponding number: ");
+		}
+		internal static void StackMenu(string connectionString) 
+		{
+			DataAccess.ShowStackNames(connectionString);
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Input a current stack name");
+            Console.WriteLine("Or input 0 to exit input");
+            Console.WriteLine("---------------------------");
+		}
+		internal static void DetailedStackMenu(string stackNameMenu)
+		{
+			Console.Clear();
+			Console.WriteLine("---------------------------");
+			Console.WriteLine($"Current working stack: {stackNameMenu}");
+            Console.WriteLine();
+			Console.WriteLine("0 to return to main menu");
+            Console.WriteLine("1 to change current stack");
+            Console.WriteLine("2 to view all Flashcards in this stack");
+            Console.WriteLine("3 to view X amount of cards in stack");
+            Console.WriteLine("4 to Create a Flashcard in current stack");
+			Console.WriteLine("5 to Edit a Flashcard");
+			Console.WriteLine("6 to Delete a Flashcard");
+			Console.WriteLine("---------------------------");
+		}
+	}
+}
--- Furiax.FlashCards/FlashCards/UserInput.cs
+++ Furiax.FlashCards/FlashCards/UserInput.cs
@@ -1,22 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using FlashCards.Model;
 namespace FlashCards
 {
 	internal class UserInput
 	{
-		internal static void ShowMenu()
+        internal static void GetMenuInput(string connectionString)
+        {
+			bool closeApp = false;
+			while (!closeApp)
+			{
+				Helpers.MainMenu();
+				string menuChoice = Console.ReadLine();
+				switch (menuChoice)
+				{
+					case "1":
+						DataAccess.Stacks(connectionString); Console.ReadLine(); Console.Clear();
+						break;
+					case "2":
+						DataAccess.Flashcards(); Console.ReadLine(); Console.Clear();
+						break;
+					case "3":
+						/*Study()*/
+						; Console.ReadLine(); Console.Clear();
+						break;
+					case "4":
+						/*ViewStudyData()*/
+						; Console.ReadLine(); Console.Clear();
+						break;
+					case "0":
+						closeApp = true;
+						Console.WriteLine("Goodbye");
+						Environment.Exit(0);
+						break;
+					default:
+						Console.WriteLine("Invalid input");
+						Console.ReadLine();
+						Console.Clear();
+						break;
+				} 
+			}
+		}
+		internal static void GetStackInput(string connectionString)
 		{
-            Console.WriteLine("Flashcards");
-            Console.WriteLine("----------");
-			Console.WriteLine("1. Manage Stacks");
-            Console.WriteLine("2. Manage Flashcards");
-            Console.WriteLine("3. Study");
-            Console.WriteLine("4. View Study Data");
-            Console.WriteLine("0. Exit");
-        }
+
+			while (true)
+			{
+				string input = Console.ReadLine();
+				bool isInputInStackNames = false;
+				string stackNameMenu = "";
+				List<StackNameDTO> stackNames = DataAccess.BuildStackDTO(connectionString);
+
+				foreach (StackNameDTO stackName in stackNames)
+				{
+					if (input.ToLower() == stackName.StackName.ToLower())
+					{
+						isInputInStackNames = true;
+						stackNameMenu = stackName.StackName;
+					}
+				}
+
+				if (isInputInStackNames)
+				{
+					Helpers.DetailedStackMenu(stackNameMenu);
+					break;
+				}
+				else
+				{
+					if (input == "0")
+					{
+						Console.Clear(); GetMenuInput(connectionString);break;
+					}
+					else
+					{
+						Console.WriteLine("Invalid choice");
+					}
+				} 
+			}
+		}
 	}
 }
