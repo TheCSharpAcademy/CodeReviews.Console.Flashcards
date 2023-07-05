b1bc152749c63e41277e741c08c34f225bc98e98
furiax <carlmalfliet@proximus.be>
2023-07-05 21:10:28 +0200

    Working on the menu layout

--- Furiax.FlashCards/FlashCards/DataAccess.cs
+++ Furiax.FlashCards/FlashCards/DataAccess.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConsoleTableExt;
+using FlashCards.Model;
+using System.Data.SqlClient;
 
 namespace FlashCards
 {
 	internal class DataAccess
 	{
+		internal static void Flashcards()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal static void ShowStackNames(string connectionString)
+		{
+			List<StackNameDTO> stackNames = BuildStackDTO(connectionString);
+			ConsoleTableBuilder
+					.From(stackNames)
+					.WithColumn("Overview Stack Names")
+					.ExportAndWriteLine();
+            }
+
+		internal static List<StackNameDTO> BuildStackDTO(string connectionString)
+		{
+			List<StackNameDTO> stackNames = new();
+			using (var connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
+				var sqlCommand = connection.CreateCommand();
+				sqlCommand.CommandText = "SELECT StackName FROM dbo.Stack";
+
+				SqlDataReader reader = sqlCommand.ExecuteReader();
+				if (reader.HasRows)
+				{
+					stackNames.Clear();
+					while (reader.Read())
+					{
+						stackNames.Add(new StackNameDTO
+						{
+							StackName = reader.GetString(0)
+						});
+					}
+				}
+				else
+					Console.WriteLine("No records found");
+				connection.Close();
+			}
+			return stackNames;
+	}
+
+		internal static void Stacks(string connectionString)
+		{
+			Console.Clear();
+			Helpers.StackMenu(connectionString);
+			UserInput.GetStackInput(connectionString);
+			
+		}
 	}
 }
--- Furiax.FlashCards/FlashCards/Program.cs
+++ Furiax.FlashCards/FlashCards/Program.cs
@@ -4,22 +4,23 @@ using FlashCards;
 
 var connectionString = ConfigurationManager.ConnectionStrings["Flashcards"].ConnectionString;
 
-UserInput.ShowMenu();
+Console.Title = "Flashcards";
+UserInput.GetMenuInput(connectionString);
 
-using (SqlConnection connection = new SqlConnection(connectionString))
-{
-	try
-	{
-		connection.Open();
-		if (connection.State == System.Data.ConnectionState.Open)
-		{
-            Console.WriteLine("connection succesfull");
-        }
-		else
-		{ Console.WriteLine("connection failed"); }
-	}
-	catch (Exception ex)
-	{
-        Console.WriteLine("An error occured: " + ex);
-    }
-}
+//using (SqlConnection connection = new SqlConnection(connectionString))
+//{
+//	try
+//	{
+//		connection.Open();
+//		if (connection.State == System.Data.ConnectionState.Open)
+//		{
+//			Console.WriteLine("connection succesfull");
+//		}
+//		else
+//		{ Console.WriteLine("connection failed"); }
+//	}
+//	catch (Exception ex)
+//	{
+//		Console.WriteLine("An error occured: " + ex);
+//	}
+//}
