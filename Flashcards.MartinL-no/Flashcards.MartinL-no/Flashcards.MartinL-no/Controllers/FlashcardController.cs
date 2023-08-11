e9e0dac69c853cbf5fbbdf07473e890705662ba2
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-11 14:16:39 +0200

    feature[FlashcardController]: add create stack method

--- Flashcards.MartinL-no/Flashcards.MartinL-no/Flashcards.MartinL-no/Controllers/FlashcardController.cs
+++ Flashcards.MartinL-no/Flashcards.MartinL-no/Flashcards.MartinL-no/Controllers/FlashcardController.cs
@@ -20,4 +20,15 @@ internal class FlashcardController
     {
         return _stackRepo.GetStackByName(name);
     }
+
+    public bool CreateStack(string name)
+    {
+        if (!string.IsNullOrWhiteSpace(name))
+        {
+            var stack = new FlashcardStack(name, new Stack<string>());
+            return _stackRepo.InsertStack(stack);
+        }
+
+        return false;
+    }
 }
