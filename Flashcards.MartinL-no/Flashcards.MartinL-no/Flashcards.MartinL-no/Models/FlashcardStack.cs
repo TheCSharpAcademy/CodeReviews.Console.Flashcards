2f13d4ca1e22b4550d0f20f407187c188e845788
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-11 11:34:25 +0200

    feat[FlashcardStackRepository]:  add get all and get by name methods

---
+++ Flashcards.MartinL-no/Flashcards.MartinL-no/Flashcards.MartinL-no/Models/FlashcardStack.cs
@@ -0,0 +1,15 @@
+﻿namespace Flashcards.MartinL_no.DAL.Models;
+
+internal class FlashcardStack
+{
+    public readonly int Id;
+    public readonly string Name;
+    public readonly Stack<string> Flashcards;
+
+    public FlashcardStack(int id, string name, Stack<string> flashcards)
+    {
+        Id = id;
+        Name = name;
+        Flashcards = flashcards;
+    }
+}
