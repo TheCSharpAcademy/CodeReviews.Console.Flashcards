26aed02e5d9e9cd3626162573530472fe9912de2
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-13 19:31:02 +0200

    feature[user interface]: move stack/flashcard management functionality to StackManager class

--- Flashcards.MartinL-no/Flashcards.MartinL-no/Flashcards.MartinL-no/Program.cs
+++ Flashcards.MartinL-no/Flashcards.MartinL-no/Flashcards.MartinL-no/Program.cs
@@ -2,10 +2,12 @@
 using Flashcards.MartinL_no.Controllers;
 using Flashcards.MartinL_no.UserInterface;
 
-
 var connectionString = System.Configuration.ConfigurationManager.AppSettings.Get("connectionString");
+
 var stackRepo = new FlashcardStackRepository(connectionString);
-var controller = new FlashcardController(stackRepo);
-var ui = new UserInput(controller);
+var stackManagerController = new StackManagerController(stackRepo);
+var stackManager = new StackManager(stackManagerController);
+
+var ui = new UserInput(stackManager);
 
 ui.Menu();
