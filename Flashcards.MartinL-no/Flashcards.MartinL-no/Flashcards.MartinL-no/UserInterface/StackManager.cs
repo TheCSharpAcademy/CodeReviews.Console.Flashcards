e906953fc2303d13ba5b15ec9afa58ab1196bdf2
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-13 19:31:02 +0200

    feature[user interface]: move stack/flashcard management functionality to StackManager class

---
+++ Flashcards.MartinL-no/Flashcards.MartinL-no/Flashcards.MartinL-no/UserInterface/StackManager.cs
@@ -0,0 +1,291 @@
+﻿using Flashcards.MartinL_no.Controllers;
+
+namespace Flashcards.MartinL_no.UserInterface;
+
+internal class StackManager
+{
+    private readonly StackManagerController _stackManagerController;
+
+    public StackManager(StackManagerController stackManagerController)
+	{
+        _stackManagerController = stackManagerController;
+    }
+
+    public void ManageStacks()
+    {
+        while (true)
+        {
+
+            Console.Clear();
+
+            var stackNames = _stackManagerController.GetStackNames();
+            TableVisualizationEngine.ShowTable(stackNames);
+
+            Helpers.ShowLine();
+            Console.WriteLine("""
+                0 to exit
+                C to create Stack
+                D to delete Stack
+                """);
+            Helpers.ShowLine();
+
+            var op = Console.ReadLine();
+
+            switch (op.ToUpper())
+            {
+                case "0":
+                    return;
+                case "C":
+                    CreateStack();
+                    break;
+                case "D":
+                    DeleteStack();
+                    break;
+                default:
+                    Helpers.ShowMessage("Invalid input, please try again");
+                    break;
+            }
+        }
+    }
+
+    private void CreateStack()
+    {
+        while (true)
+        {
+            Console.Clear();
+            var name = Helpers.Ask("Enter the name of the stack you would like to create (or 0 to exit): ");
+            if (name == "0") return;
+
+            if (String.IsNullOrWhiteSpace(name))
+            {
+                Helpers.ShowMessage("You must enter a name, please try again");
+            }
+            else if (_stackManagerController.CreateStack(name))
+            {
+                Helpers.ShowMessage("Stack added!");
+            }
+            else
+            {
+                Helpers.ShowMessage("Stack already exists, please try again");
+            }
+        }
+    }
+
+    private void DeleteStack()
+    {
+        while (true)
+        {
+            Console.Clear();
+            var name = Helpers.Ask("Enter the name of the stack you would like to delete (or 0 to exit): ");
+            if (name == "0") return;
+
+            if (String.IsNullOrWhiteSpace(name))
+            {
+                Helpers.ShowMessage("You must enter a name, please try again");
+            }
+            else if (_stackManagerController.DeleteStack(name))
+            {
+                Helpers.ShowMessage("Stack deleted!");
+                return;
+            }
+            else
+            {
+                Helpers.ShowMessage("Stack does not exist, please try again");
+            }
+        }
+    }
+
+    public void ManageFlashcards()
+    {
+        var stackNames = _stackManagerController.GetStackNames();
+
+        while (true)
+        {
+            Console.Clear();
+
+            TableVisualizationEngine.ShowTable(stackNames);
+
+            Console.WriteLine("""
+                Choose a stack of flashcards to interact with:
+
+                """);
+
+            Helpers.ShowLine();
+            Console.WriteLine("""
+                Input a current stack name
+                or input 0 to exit input
+                """);
+            Helpers.ShowLine();
+
+            var stackName = Console.ReadLine();
+            if (stackName == "0") break;
+
+            var isValidStackName = stackNames.Exists(s => s.ToLower() == stackName.ToLower());
+            if (isValidStackName)
+            {
+                StackMenu(stackName);
+                break;
+            }
+            else Helpers.ShowMessage("Invalid input, please try again");
+        }
+    }
+
+    private void StackMenu(string stackName)
+    {
+        while (true)
+        {
+            Console.Clear();
+
+            Helpers.ShowLine();
+            Console.WriteLine($"""
+                Current working stack: {stackName}
+
+                0 to return to main menu
+                X to change current stack
+                V to view all Flashcards in stack
+                A to view X amount of cards in stack
+                C to create a Flashcard in current stack
+                E to edit a Flashcard
+                D to delete a Flashcard
+                """);
+            Helpers.ShowLine();
+
+            var op = Console.ReadLine();
+
+            switch (op.ToUpper())
+            {
+                case "0":
+                    return;
+                case "X":
+                    ManageFlashcards();
+                    break;
+                case "V":
+                    ViewAllFlashCards(stackName);
+                    break;
+                case "A":
+                    var amountString = Helpers.Ask("How many Flashcards do you want to see: ");
+                    int amount;
+                    if (!Int32.TryParse(amountString, out amount)) goto default;
+                    ViewFlashcards(stackName, amount);
+                    break;
+                case "C":
+                    CreateFlashcard(stackName);
+                    break;
+                case "E":
+                    EditFlashcard(stackName);
+                    break;
+                case "D":
+                    DeleteFlashcard(stackName);
+                    break;
+                default:
+                    Helpers.ShowMessage("Invalid input, please try again");
+                    break;
+            }
+        }
+    }
+
+    private void ViewAllFlashCards(string stackName)
+    {
+        Console.Clear();
+
+        var stack = _stackManagerController.GetStackByName(stackName);
+        TableVisualizationEngine.ShowTable(stack);
+
+        Helpers.ShowLine();
+        Helpers.Ask("Press any key to return to menu: ");
+        Helpers.ShowLine();
+
+    }
+
+    private void ViewFlashcards(string stackName, int amount)
+    {
+        Console.Clear();
+
+        var stack = _stackManagerController.GetStackByName(stackName);
+        var flashcards = stack.Flashcards.Take(amount).ToList();
+        stack.Flashcards = flashcards;
+        TableVisualizationEngine.ShowTable(stack);
+
+        Helpers.ShowLine();
+        Helpers.Ask("Press any key to return to menu: ");
+        Helpers.ShowLine();
+    }
+
+    private void CreateFlashcard(string stackName)
+    {
+        while (true)
+        {
+            Console.Clear();
+            var front = Helpers.Ask("What should the front be? ");
+            var back = Helpers.Ask("What should the back be? ");
+
+            var stack = _stackManagerController.GetStackByName(stackName);
+            var isAdded = _stackManagerController.CreateFlashcard(front, back, stack.Id);
+
+            if (isAdded)
+            {
+                Helpers.ShowMessage("Flashcard created!");
+                break;
+            }
+
+            Helpers.ShowMessage("Invalid entry, please try again");
+        }
+    }
+
+    private void EditFlashcard(string stackName)
+    {
+        while (true)
+        {
+            Console.Clear();
+
+            var stack = _stackManagerController.GetStackByName(stackName);
+            TableVisualizationEngine.ShowTable(stack);
+
+            var viewIdString = Helpers.Ask("What is the id of the flashcard you want to edit: ");
+            var front = Helpers.Ask("What should the front be: ");
+            var back = Helpers.Ask("What should the back be: ");
+            int viewId;
+
+            if (Int32.TryParse(viewIdString, out viewId))
+            {
+                var id = stack.Flashcards[viewId - 1].Id;
+                var isAdded = _stackManagerController.UpdateFlashcard(id, front, back, stack.Id);
+                if (isAdded)
+                {
+                    Helpers.ShowMessage("Flashcard updated!");
+                    return;
+                }
+            }
+
+            else Helpers.ShowMessage("Invalid entry, please try again");
+        }
+    }
+
+    private void DeleteFlashcard(string stackName)
+    {
+        while (true)
+        {
+            Console.Clear();
+
+            var stack = _stackManagerController.GetStackByName(stackName);
+            TableVisualizationEngine.ShowTable(stack);
+
+            var viewIdString = Ask("What is the id of the flashcard you want to delete: ");
+            int viewId;
+
+            if (Int32.TryParse(viewIdString, out viewId))
+            {
+                var id = stack.Flashcards[viewId - 1].Id;
+                var isAdded = _stackManagerController.DeleteFlashcard(id);
+                if (isAdded)
+                {
+                    Helpers.ShowMessage("Flashcard deleted!");
+                    return;
+                }
+            }
+
+            else Helpers.ShowMessage("Invalid entry, please try again");
+        }
+    }
+}
+
--- Flashcards.MartinL-no/Flashcards.MartinL-no/Flashcards.MartinL-no/UserInterface/UserInput.cs
+++ Flashcards.MartinL-no/Flashcards.MartinL-no/Flashcards.MartinL-no/UserInterface/UserInput.cs
@@ -4,11 +4,11 @@ namespace Flashcards.MartinL_no.UserInterface;
 
 internal class UserInput
 {
-    private readonly FlashcardController _controller;
+    private readonly StackManager _stackManager;
 
-	public UserInput(FlashcardController controller)
+	public UserInput(StackManager stackManager)
 	{
-        _controller = controller;
+        _stackManager = stackManager;
     }
 
     public void Menu()
@@ -16,7 +16,7 @@ internal class UserInput
         while (true)
         {
             Console.Clear();
-            ShowLine();
+            Helpers.ShowLine();
             Console.WriteLine("""
                 0 - exit
                 S - Manage Stacks
@@ -24,17 +24,17 @@ internal class UserInput
                 ST - Study
                 V - View study session data
                 """);
-            ShowLine();
+            Helpers.ShowLine();
 
             var op = Console.ReadLine();
 
             switch (op.ToUpper())
             {
                 case "S":
-                    ManageStacks();
+                    _stackManager.ManageStacks();
                     break;
                 case "F":
-                    ManageFlashcards();
+                    _stackManager.ManageFlashcards();
                     break;
                 case "ST":
                     Study();
@@ -43,289 +43,12 @@ internal class UserInput
                     ViewStudySessionData();
                     break;
                 case "0":
-                    ShowMessage("Program ended");
+                    Helpers.ShowMessage("Program ended");
                     return;
                 default:
-                    ShowMessage("Invalid input, please try again");
+                    Helpers.ShowMessage("Invalid input, please try again");
                     break;
             }
-        }
-    }
-
-    private void ManageStacks()
-    {
-        while (true)
-        {
-
-            Console.Clear();
-
-            var stackNames = _controller.GetStackNames();
-            TableVisualizationEngine.ShowTable(stackNames);
-
-            ShowLine();
-            Console.WriteLine("""
-                0 to exit
-                C to create Stack
-                D to delete Stack
-                """);
-            ShowLine();
-
-            var op = Console.ReadLine();
-
-            switch (op.ToUpper())
-            {
-                case "0":
-                    return;
-                case "C":
-                    CreateStack();
-                    break;
-                case "D":
-                    DeleteStack();
-                    break;
-                default:
-                    ShowMessage("Invalid input, please try again");
-                    break;
-            }
-        }
-    }
-
-    private void CreateStack()
-    {
-        while (true)
-        {
-            Console.Clear();
-            var name = Ask("Enter the name of the stack you would like to create (or 0 to exit): ");
-            if (name == "0") return;
-
-            if (String.IsNullOrWhiteSpace(name))
-            {
-                ShowMessage("You must enter a name, please try again");
-            }
-            else if (_controller.CreateStack(name))
-            {
-                ShowMessage("Stack added!");
-            }
-            else
-            {
-                ShowMessage("Stack already exists, please try again");
-            }
-        }
-    }
-
-    private void DeleteStack()
-    {
-        while (true)
-        {
-            Console.Clear();
-            var name = Ask("Enter the name of the stack you would like to delete (or 0 to exit): ");
-            if (name == "0") return;
-
-            if (String.IsNullOrWhiteSpace(name))
-            {
-                ShowMessage("You must enter a name, please try again");
-            }
-            else if (_controller.DeleteStack(name))
-            {
-                ShowMessage("Stack deleted!");
-                return;
-            }
-            else
-            {
-                ShowMessage("Stack does not exist, please try again");
-            }
-        }
-    }
-
-    private void ManageFlashcards()
-    {
-        var stackNames = _controller.GetStackNames();
-
-        while (true)
-        {
-            Console.Clear();
-
-            TableVisualizationEngine.ShowTable(stackNames);
-
-            Console.WriteLine("""
-                Choose a stack of flashcards to interact with:
-
-                """);
-
-            ShowLine();
-            Console.WriteLine("""
-                Input a current stack name
-                or input 0 to exit input
-                """);
-            ShowLine();
-
-            var stackName = Console.ReadLine();
-            if (stackName == "0") break;
-
-            var isValidStackName = stackNames.Exists(s => s.ToLower() == stackName.ToLower());
-            if (isValidStackName)
-            {
-                StackMenu(stackName);
-                break;
-            }
-            else ShowMessage("Invalid input, please try again");
-        }
-    }
-
-    private void StackMenu(string stackName)
-    {
-        while (true)
-        {
-            Console.Clear();
-
-            ShowLine();
-            Console.WriteLine($"""
-                Current working stack: {stackName}
-
-                0 to return to main menu
-                X to change current stack
-                V to view all Flashcards in stack
-                A to view X amount of cards in stack
-                C to create a Flashcard in current stack
-                E to edit a Flashcard
-                D to delete a Flashcard
-                """);
-            ShowLine();
-
-            var op = Console.ReadLine();
-
-            switch (op.ToUpper())
-            {
-                case "0":
-                    return;
-                case "X":
-                    ManageFlashcards();
-                    break;
-                case "V":
-                    ViewAllFlashCards(stackName);
-                    break;
-                case "A":
-                    var amountString = Ask("How many Flashcards do you want to see: ");
-                    int amount;
-                    if (!Int32.TryParse(amountString, out amount)) goto default;
-                    ViewFlashcards(stackName, amount);
-                    break;
-                case "C":
-                    CreateFlashcard(stackName);
-                    break;
-                case "E":
-                    EditFlashcard(stackName);
-                    break;
-                case "D":
-                    DeleteFlashcard(stackName);
-                    break;
-                default:
-                    ShowMessage("Invalid input, please try again");
-                    break;
-            }
-        }
-    }
-
-    private void ViewAllFlashCards(string stackName)
-    {
-        Console.Clear();
-
-        var stack = _controller.GetStackByName(stackName);
-        TableVisualizationEngine.ShowTable(stack);
-
-        ShowLine();
-        Ask("Press any key to return to menu: ");
-        ShowLine();
-
-    }
-
-    private void ViewFlashcards(string stackName, int amount)
-    {
-        Console.Clear();
-
-        var stack = _controller.GetStackByName(stackName);
-        var flashcards = stack.Flashcards.Take(amount).ToList();
-        stack.Flashcards = flashcards;
-        TableVisualizationEngine.ShowTable(stack);
-
-        ShowLine();
-        Ask("Press any key to return to menu: ");
-        ShowLine();
-    }
-
-    private void CreateFlashcard(string stackName)
-    {
-        while (true)
-        {
-            Console.Clear();
-            var front = Ask("What should the front be? ");
-            var back = Ask("What should the back be? ");
-
-            var stack = _controller.GetStackByName(stackName);
-            var isAdded = _controller.CreateFlashcard(front, back, stack.Id);
-
-            if (isAdded)
-            {
-                ShowMessage("Flashcard created!");
-                break;
-            }
-
-            ShowMessage("Invalid entry, please try again");
-        }
-    }
-
-    private void EditFlashcard(string stackName)
-    {
-        while (true)
-        {
-            Console.Clear();
-
-            var stack = _controller.GetStackByName(stackName);
-            TableVisualizationEngine.ShowTable(stack);
-
-            var viewIdString = Ask("What is the id of the flashcard you want to edit: ");
-            var front = Ask("What should the front be: ");
-            var back = Ask("What should the back be: ");
-            int viewId;
-
-            if (Int32.TryParse(viewIdString, out viewId))
-            {
-                var id = stack.Flashcards[viewId - 1].Id;
-                var isAdded = _controller.UpdateFlashcard(id, front, back, stack.Id);
-                if (isAdded)
-                {
-                    ShowMessage("Flashcard updated!");
-                    return;
-                }
-            }
-
-            else ShowMessage("Invalid entry, please try again");
-        }
-    }
-
-    private void DeleteFlashcard(string stackName)
-    {
-        while (true)
-        {
-            Console.Clear();
-
-            var stack = _controller.GetStackByName(stackName);
-            TableVisualizationEngine.ShowTable(stack);
-
-            var viewIdString = Ask("What is the id of the flashcard you want to delete: ");
-            int viewId;
-
-            if (Int32.TryParse(viewIdString, out viewId))
-            {
-                var id = stack.Flashcards[viewId - 1].Id;
-                var isAdded = _controller.DeleteFlashcard(id);
-                if (isAdded)
-                {
-                    ShowMessage("Flashcard deleted!");
-                    return;
-                }
-            }
-
-            else ShowMessage("Invalid entry, please try again");
         }
     }
 
@@ -337,23 +60,5 @@ internal class UserInput
     private void ViewStudySessionData()
     {
         throw new NotImplementedException();
-    }
-
-    private void ShowMessage(string message)
-    {
-        Console.Clear();
-        Console.WriteLine(message);
-        Thread.Sleep(2500);
-    }
-
-    private void ShowLine()
-    {
-        Console.WriteLine("---------------------------------");
-    }
-
-    private string Ask(string message)
-    {
-        Console.Write(message);
-        return Console.ReadLine();
     }
 }
