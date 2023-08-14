c66ac610edeb71b485752fe7a4519e532b05b352
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-14 12:08:06 +0200

    feature[study session controller]: add get/ add session methods

--- Flashcards.MartinL-no/Flashcards.MartinL-no/Flashcards.MartinL-no/Controllers/StudySessionController.cs
+++ Flashcards.MartinL-no/Flashcards.MartinL-no/Flashcards.MartinL-no/Controllers/StudySessionController.cs
@@ -1,4 +1,7 @@
-﻿namespace Flashcards.MartinL_no.Controllers;
+﻿using Flashcards.MartinL_no.DAL;
+using Flashcards.MartinL_no.Models;
+
+namespace Flashcards.MartinL_no.Controllers;
 
 internal class StudySessionController
 {
@@ -7,5 +10,28 @@ internal class StudySessionController
     public StudySessionController(StudySessionRepository sessionRepo)
     {
         _sessionRepo = sessionRepo;
+    }
+
+    public List<StudySession> GetSessions()
+    {
+        return _sessionRepo.GetSessions();
+    }
+
+    public List<StudySession> GetSessionByName(string name)
+    {
+        return _sessionRepo.GetSessionByName(name);
+    }
+
+    public bool AddStudySession(DateTime date, int score, int stackId)
+    {
+        if (score < 0) return false;
+
+        var session = new StudySession()
+        {
+            Date = DateOnly.FromDateTime(date),
+            Score = score,
+            StackId = stackId
+        };
+        return _sessionRepo.InsertStudySession(session);
     }
 }
