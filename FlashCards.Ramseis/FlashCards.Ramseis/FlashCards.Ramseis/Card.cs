051320c2a74d8b18b715b5abae454d7c042e46d1
Schmit <nickvschmit@yahoo.com>
2023-08-28 17:52:31 -0700

    Initial commit

---
+++ FlashCards.Ramseis/FlashCards.Ramseis/FlashCards.Ramseis/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace FlashCards.Ramseis
+{
+    internal class Card
+    {
+        public int ID { get; set; }
+        public string Question { get; set; } = String.Empty;
+        public string Answer { get; set; } = String.Empty;
+    }
+}
---
+++ FlashCards.Ramseis/FlashCards.Ramseis/FlashCards.Ramseis/History.cs
@@ -0,0 +1,12 @@
+﻿namespace FlashCards.Ramseis
+{
+    internal class History
+    {
+        public static void Menu()
+        {
+            ConsoleTable.PrintTable(IO.SqlGetHistory());
+            Console.Write("\nPress any key to return to main menu...");
+            Console.ReadKey();
+        }
+    }
+}
