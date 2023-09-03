7d02d6fdc7b3c1f05f4a6273c8c3c3ebef052e56
Pablo Martinez Buesa <rekshooter@gmail.com>
2023-09-03 20:48:13 +0200

    Project finished

---
+++ Flashcards.w0lvesvvv/Flashcards.w0lvesvvv/DTOs/StudySessionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Flashcards.w0lvesvvv.DTOs
+{
+    public class StudySessionDTO
+    {
+        public DateTime StudySessionDate { get; set; }
+        public int StudySessionPoints { get; set; }
+        public int StudySessionMaxPoints { get; set; }
+    }
+}
