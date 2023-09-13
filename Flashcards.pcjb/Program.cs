89a3c23ab858bf52fc597ccecdb1ccf4acc61ac2
pcjb <143715460+pcjb@users.noreply.github.com>
2023-09-13 21:04:47 +0200

    Database setup, config, logger

--- Flashcards.pcjb/Program.cs
+++ Flashcards.pcjb/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        var config = new Configuration();
+
+        Logger.Info("Application started.");
         Console.WriteLine("Flashcards");
+        DatabaseSetup.CreateTablesIfNotPresent(config.DatabaseConnectionString);
     }
 }
