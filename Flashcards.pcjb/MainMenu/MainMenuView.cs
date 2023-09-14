5753c66cb7650d4f618f07befd76ac118c26edef
pcjb <143715460+pcjb@users.noreply.github.com>
2023-09-14 09:22:33 +0200

    View, MainMenu setup

---
+++ Flashcards.pcjb/MainMenu/MainMenuView.cs
@@ -0,0 +1,27 @@
+namespace Flashcards;
+
+class MainMenuView : BaseView
+{
+    private MainMenuController controller;
+
+    public MainMenuView(MainMenuController controller)
+    {
+        this.controller = controller;
+    }
+
+    public override void Body()
+    {
+        Console.WriteLine("0 - Exit");
+        Console.WriteLine("Enter one of the numbers above to select a menu option.");
+
+        switch (Console.ReadKey().KeyChar.ToString())
+        {
+            case "0":
+                controller.Exit();
+                break;
+            default:
+                controller.ShowMainMenu();
+                break;
+        }
+    }
+}
