c30366dd509fbccb6208594526747bd0d8a38ded
Marcus Eklund <marckus@gmail.com>
2023-09-27 17:16:02 +0200

    First Push
    Still working on it. Only partial Model/DTO done.

---
+++ FlashCards.Forser/DTOs/FlashCardDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace FlashCards.Forser.DTOs
+{
+    public class FlashCardDTO
+    {
+        public string? Front { get; set; }
+        public string? Back { get; set; }
+
+        public FlashCardDTO(string? front, string? back) 
+        {
+            Front = front;
+            Back = back;
+        }
+    }
+}
