7e6eff60b7a995064f2f76ea04c58d89d7510340
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-10-14 20:53:34 +0200

    Refactor code

--- Flashcards.wkktoria/Flashcards.wkktoria/Models/Card.cs
+++ Flashcards.wkktoria/Flashcards.wkktoria/Models/Card.cs
@@ -3,6 +3,7 @@ namespace Flashcards.wkktoria.Models;
 internal class Card
 {
     public int Id { get; set; }
+    public int StackId { get; set; }
     public string Front { get; set; } = string.Empty;
     public string Back { get; set; } = string.Empty;
 }
