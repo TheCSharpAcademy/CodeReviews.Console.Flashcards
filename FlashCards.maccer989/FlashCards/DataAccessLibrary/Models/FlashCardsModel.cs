8305cfa1a05dfa658b153542102770e3d849857b
maccer989 <132687553+maccer989@users.noreply.github.com>
2023-10-16 20:20:35 +0100

    Project Completed
    Project Completed

---
+++ FlashCards.maccer989/FlashCards/DataAccessLibrary/Models/FlashCardsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccessLibrary.Models
+{
+    public class FlashCardsModel
+    {
+        public int id { get; set; }
+        public int StackName { get; set; }
+        public int FlashCardId { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
