8f44f08be762bf4d08893509d56778869665970f
ugnius <ugnius.vilkoncius@gmail.com>
2023-12-11 11:19:15 +0200

    Initial

---
+++ Flashcards.UgniusFalze/Flashcards.UgniusFalze/Models/FlashcardDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace Flashcards.UgniusFalze.Models;
+
+public class FlashcardDTO
+{
+    private int FlashcardId {  get; set; }
+    public int Order { get; set; }
+    public string Front { get; set; }
+    public string Back { get; set; }
+
+    public FlashcardDTO(int flashcardId, string front, string back, int order)
+    {
+        FlashcardId = flashcardId;
+        Front = front;
+        Back = back;
+        Order = order;
+    }
+
+    public int GetActualId()
+    {
+        return FlashcardId;
+    }
+}
---
+++ Flashcards.UgniusFalze/Flashcards.UgniusFalze/readme.md
@@ -0,0 +1,5 @@
+﻿## Before Running 
+To get started make sure you set up the correct connection string for your local database in `app.config` file in project root. 
+After that modify the included `databaseCreateScript.txt` to your specified local database file and then run the modified script.
+## Packages used
+As with previous projects, I used ConsoleTableExt for displaying table data such as stacks and flashcards. 
