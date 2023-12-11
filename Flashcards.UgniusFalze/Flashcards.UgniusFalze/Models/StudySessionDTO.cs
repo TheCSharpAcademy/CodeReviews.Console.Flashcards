3efe12859ba53970dc7115988bcf671d750731a7
ugnius <ugnius.vilkoncius@gmail.com>
2023-12-11 11:19:15 +0200

    Initial

---
+++ Flashcards.UgniusFalze/Flashcards.UgniusFalze/Models/StudySessionDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Flashcards.UgniusFalze.Models;
+
+public class StudySessionDTO
+{
+    public DateTime Date { get; set; }
+    public int Score { get; set; }
+    public string StackName { get; set; }
+    
+    public StudySessionDTO(DateTime date, int score, string stackName)
+    {
+        Date = date;
+        Score = score;
+        StackName = stackName;
+    }
+}
---
+++ Flashcards.UgniusFalze/Flashcards.UgniusFalze/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Flashcards.UgniusFalze;using Flashcards.UgniusFalze.Menu;
+using System.Configuration;
+
+try
+{
+    string? connString = ConfigurationManager.AppSettings.Get("ConnString");
+    if (connString == null)
+    {
+        Console.WriteLine("Connection string is missing");
+        return;
+    }
+    Driver driver = new Driver(connString);
+
+    DisplayController displayController = new DisplayController(driver);
+    new Menu("Choose what you want to do")
+        .AddOption("Manage stacks", () => displayController.ManageStacks())
+        .AddOption("Add stack", () => displayController.AddStack())
+        .AddOption("Check study sessions", () => displayController.DisplaySessions())
+        .AddOption("Study", () => displayController.Study())
+        .Display();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
