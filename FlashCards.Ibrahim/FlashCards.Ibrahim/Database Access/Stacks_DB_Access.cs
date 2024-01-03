2dd6ee7b8c78688508acf78d538d110a1e08d3c8
Ibrahim Gilani <ibigilani@gmail.com>
2024-01-03 00:41:52 -0500

    added database creation code and models

---
+++ FlashCards.Ibrahim/FlashCards.Ibrahim/Database Access/Stacks_DB_Access.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlashCards.Ibrahim.Models;
+
+namespace FlashCards.Ibrahim.Database_Access
+{
+    internal class Stacks_DB_Access
+    {
+        string _connectionString;
+        public Stacks_DB_Access(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        public static void Insert_Stack(string connectionString)
+        {
+
+        }
+        public static void Update_Stack(string connectionString)
+        {
+        }
+        public static void Delete_Stack(string connectionString)
+        {
+
+        }
+        public static Stacks Get_Stack(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FlashCards.Ibrahim/FlashCards.Ibrahim/Models/Flashcard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards.Ibrahim.Models
+{
+    internal class Flashcard
+    {
+        public int Id { get; set; }
+        public int Stacks_Id { get; set; }
+        public string Front {  get; set; }
+        public string Back { get; set; }
+
+    }
+}
