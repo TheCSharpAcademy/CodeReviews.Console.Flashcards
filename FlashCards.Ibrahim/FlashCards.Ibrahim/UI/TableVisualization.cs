de01cb77765da707a9b0f1b1cdf783c818b14482
Ibrahim Gilani <ibigilani@gmail.com>
2024-01-04 16:00:16 -0500

    finished requirements

--- FlashCards.Ibrahim/FlashCards.Ibrahim/UI/TableVisualization.cs
+++ FlashCards.Ibrahim/FlashCards.Ibrahim/UI/TableVisualization.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ConsoleTableExt;
+using FlashCards.Ibrahim.Models;
 namespace FlashCards.Ibrahim.UI
 {
     public class TableVisualization
     {
         public static void ShowTable<T>(List<T> history) where T : class
+        {
+            ConsoleTableBuilder
+                .From(history)
+                .ExportAndWriteLine();
+        }
+
+        public static void ShowCard(List<string> history) 
         {
             ConsoleTableBuilder
                 .From(history)
