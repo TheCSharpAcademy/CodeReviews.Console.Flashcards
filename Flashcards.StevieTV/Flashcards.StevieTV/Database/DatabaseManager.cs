8edaa144918f08c98f2678a26907a84e0d316534
Stevie Robinson <stevie.robinson@gmail.com>
2024-01-06 11:54:37 +0100

    refactor database manager with a constructor update app.config to allow for connection to master and flashcards database define Stacks and Cards tables and create if not existing

--- Flashcards.StevieTV/Flashcards.StevieTV/Database/DatabaseManager.cs
+++ Flashcards.StevieTV/Flashcards.StevieTV/Database/DatabaseManager.cs
@@ -5,16 +5,43 @@ namespace Flashcards.StevieTV.Database;
 
 public class DatabaseManager
 {
-    private static string connectionstring = ConfigurationManager.AppSettings.Get("ConnectionString");
-    public static void CreateDatabase(string dbName)
+    private string dbName;
+    private string masterConnectionstring = ConfigurationManager.AppSettings.Get("ConnectionString");
+    private string connectionString;
+    public DatabaseManager(string _dbName)
     {
-        using (var connection = new SqlConnection(connectionstring))
+        dbName = _dbName;
+        connectionString = $"{masterConnectionstring}Initial Catalog={_dbName};";
+    }
+
+    public void CreateDatabase()
+    {
+        using (var connection = new SqlConnection(masterConnectionstring))
         {
             using (var databaseCommand = connection.CreateCommand())
             {
                 connection.Open();
                 databaseCommand.CommandText = $"If(db_id(N'{dbName}') IS NULL) CREATE DATABASE [{dbName}]";
                 databaseCommand.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void CreateTable(string tableName, string tableContents)
+    {
+        using (var connection = new SqlConnection(connectionString))
+        {
+            using (var tableCommand = connection.CreateCommand())
+            {
+                connection.Open();
+
+                tableCommand.CommandText =
+                    @$"IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[{tableName}]') AND type in (N'U'))
+                       BEGIN
+                         CREATE TABLE {tableName} ({tableContents})
+                       END";
+
+                tableCommand.ExecuteNonQuery();
             }
         }
     }
--- Flashcards.StevieTV/Flashcards.StevieTV/Flashcards.cs
+++ Flashcards.StevieTV/Flashcards.StevieTV/Flashcards.cs
@@ -6,9 +6,24 @@ namespace Flashcards.StevieTV;
 internal static class Flashcards
 {
     private static readonly string DatabaseName = ConfigurationManager.AppSettings.Get("Database");
-
+    private static readonly DatabaseManager DatabaseManager = new DatabaseManager(DatabaseName);
     private static void Main()
     {
-        DatabaseManager.CreateDatabase(DatabaseName);
+        DatabaseManager.CreateDatabase();
+
+        var stacksTable = @"Id int identity,
+                            Name VARCHAR(50),
+                            constraint Stacks_pk
+                                primary key (Id)";
+        
+        DatabaseManager.CreateTable("Stacks", stacksTable);
+
+        var cardsTable = @"Id int identity,
+                            StackId int Not Null,
+                            Front VARCHAR(Max) Not Null,
+                            Back VARCHAR(Max) Not Null,
+                            FOREIGN KEY (StackId) REFERENCES Stacks(Id) ON DELETE CASCADE";
+        
+        DatabaseManager.CreateTable("Cards",cardsTable);
     }
 }
