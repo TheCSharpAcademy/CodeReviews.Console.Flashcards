161fa4ac92da186ea5e3a1cf4a87f6ffa9d4e0ea
Stan Choi <sjc591@nyu.edu>
2024-01-08 00:12:54 -0500

    Test inital connection hardcoded connection into code for now

---
+++ Flashcards.StanimalTheMan/Flashcards.StanimalTheMan/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Data Source=(LocalDb)\\LocalDBDemo;Initial Catalog=Flashcards;Integrated Security=True";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+
+                // Perform database operations here
+
+                //Console.WriteLine("Connection successful!");
+
+                string selectFlashcardsQuery = $"SELECT * FROM Flashcards WHERE StackId = {1}";
+
+                using (SqlCommand command = new SqlCommand(selectFlashcardsQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("Flashcards:");
+
+                        while (reader.Read())
+                        {
+                            int flashcardId = reader.GetInt32(0);
+                            string question = reader.GetString(1);
+                            string answer = reader.GetString(2);
+
+                            Console.WriteLine($"FlashcardId: {flashcardId}, Question: {question}, Answer: {answer}");
+                        }
+                    }
+                }
+
+                // Ensure to close the connection when done
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
