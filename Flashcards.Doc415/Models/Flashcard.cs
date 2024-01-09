634f9eb515aae803a83555f41d635d71f3576dd1
doc415 <sipahioglu_serdar@yahoo.com>
2024-01-09 22:42:41 +0300

    trying again

---
+++ Flashcards.Doc415/Models/Flashcard.cs
@@ -0,0 +1,9 @@
+﻿namespace FlashCards.Doc415.Models;
+
+internal class Flashcard
+{
+    public int Id { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    public int StackId { get; set; }
+}
