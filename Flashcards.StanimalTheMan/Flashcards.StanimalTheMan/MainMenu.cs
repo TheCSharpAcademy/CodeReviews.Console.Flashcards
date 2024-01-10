58d7597f31c063615f1dd9ece6d22a7061d2d417
Stan Choi <sjc591@nyu.edu>
2024-01-10 16:39:07 -0500

    Display stacks.

---
+++ Flashcards.StanimalTheMan/Flashcards.StanimalTheMan/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using Spectre.Console;
+
+namespace Flashcards.StanimalTheMan;
+
+internal class MainMenu
+{
+    internal static void ShowMenu()
+    {
+        string[] menuOptions = new string[] { "exit", "Manage Stacks", "Manage FlashCards", "Study", "view study session data" };
+
+        bool closeMenu = false;
+        
+        while (closeMenu == false)
+        {
+            var selection = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("-----------------")
+                .PageSize(5)
+                .AddChoices(menuOptions));
+
+            switch (selection)
+            {
+                case "exit":
+                    closeMenu = true;
+                    break;
+                case "Manage Stacks":
+                    StacksInterface.ShowMenu();
+                    break;
+            }
+        }
+
+    }
+}
