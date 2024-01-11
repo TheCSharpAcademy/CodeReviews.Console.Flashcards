032187fd1081fe9d2e6b0c0bd478c6c9b7263f9e
Stan Choi <sjc591@nyu.edu>
2024-01-11 17:19:36 -0500

    Organize code better, View Stacks and Create a Stack

---
+++ Flashcards.StanimalTheMan/Flashcards.StanimalTheMan/FlashcardsInterface.cs
@@ -0,0 +1,65 @@
+﻿using Flashcards.StanimalTheMan.Models;
+using Spectre.Console;
+using System.Data.SqlClient;
+
+namespace Flashcards.StanimalTheMan;
+
+internal class FlashcardsInterface
+{
+    internal static void ShowSelectStackMenu()
+    {
+        string connectionString = "Data Source=(LocalDb)\\LocalDBDemo;Initial Catalog=Flashcards;Integrated Security=True";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            List<Stack> stacks = new List<Stack>();
+            List<string> stackSelectOptions = new List<string>();
+            stackSelectOptions.Add("Return to Main Menu");
+            try
+            {
+                connection.Open();
+
+                // Perform database operations here
+
+                //Console.WriteLine("Connection successful!");
+
+                string selectFlashcardsQuery = $"SELECT * FROM Stacks";
+
+                using (SqlCommand command = new SqlCommand(selectFlashcardsQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            int stackId = reader.GetInt32(0);
+                            string stackName = reader.GetString(1);
+
+                            stacks.Add(new Stack(stackId, stackName));
+                        }
+
+                        foreach (Stack stack in stacks)
+                        {
+                            stackSelectOptions.Add(stack.StackName);
+                        }
+
+                        var selection = AnsiConsole.Prompt(
+                            new SelectionPrompt<string>()
+                            .Title("Name")
+                            .PageSize(10)
+                            .AddChoices(stackSelectOptions));
+
+
+                    }
+                }
+
+                // Ensure to close the connection when done
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Flashcards.StanimalTheMan/Flashcards.StanimalTheMan/Models/Flashcard.cs
@@ -0,0 +1,9 @@
+﻿namespace Flashcards.StanimalTheMan.Models;
+
+internal class Flashcard
+{
+    public int FlashcardId { get; set; }
+    public string Front { get; set; }
+    public string Back { get; set; }
+    public int StackId { get; set; }
+}
