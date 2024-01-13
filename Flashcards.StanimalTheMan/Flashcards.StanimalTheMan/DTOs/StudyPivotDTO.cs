046c195823e4e244c2cc382819e5c81db6823908
Stan Choi <sjc591@nyu.edu>
2024-01-13 14:48:55 -0500

    First pass at adding num sessions per month per year, ugly format

---
+++ Flashcards.StanimalTheMan/Flashcards.StanimalTheMan/DTOs/StudyPivotDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Flashcards.StanimalTheMan.DTOs;
+
+internal class StudyPivotDTO
+{
+    public StudyPivotDTO(string stackName, List<int>monthlyValues)
+    {
+        StackName = stackName;
+        MonthlyValues = monthlyValues;
+    }
+
+    public string StackName { get; set; }
+    public List<int> MonthlyValues { get; set; }
+}
