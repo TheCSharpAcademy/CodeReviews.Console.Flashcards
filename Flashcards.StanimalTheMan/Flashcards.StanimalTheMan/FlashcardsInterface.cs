d6ddada0222c06cc76831fea06b605bc705f65ee
Stan Choi <sjc591@nyu.edu>
2024-01-13 10:10:14 -0500

    Add update flashcard impl

--- Flashcards.StanimalTheMan/Flashcards.StanimalTheMan/FlashcardsInterface.cs
+++ Flashcards.StanimalTheMan/Flashcards.StanimalTheMan/FlashcardsInterface.cs
@@ -146,12 +146,110 @@ internal class FlashcardsInterface
             case ManageFlashcardsOption.CreateAFlashcardInCurrentStack:
                 CreateAFlashcardInCurrentStack(selectedStackId);
                 break;
-            //case ManageFlashcardsOption.EditFlashcard:
-            //    EditFlashcard();
-            //    break;
+            case ManageFlashcardsOption.EditFlashcard:
+                EditFlashcard(selectedStackId);
+                break;
             case ManageFlashcardsOption.DeleteFlashcard:
                 DeleteFlashcard(selectedStackId);
                 break;
+        }
+    }
+
+    private static void EditFlashcard(int selectedStackId)
+    {
+        SqlConnection connection = null;
+
+        try
+        {
+            connection = DatabaseHelper.GetOpenConnection();
+
+            // Perform database operations here
+
+            //Console.WriteLine("Connection successful!");
+            Console.WriteLine("Select the flashcard you want to delete");
+
+            List<FlashcardDTO> flashcardDTOs = new();
+            List<string> flashcardSelectionOptions = new();
+            string fetchFlashcardsQuery = $"SELECT * FROM Flashcards WHERE StackId = @StackId";
+
+            SqlCommand getFlashcards = new SqlCommand(fetchFlashcardsQuery, connection);
+            try
+            {
+                getFlashcards.Parameters.AddWithValue("@StackId", selectedStackId);
+                using (SqlDataReader reader = getFlashcards.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int flashcardId = reader.GetInt32(0);
+                        string front = reader.GetString(1);
+                        string back = reader.GetString(2);
+
+                        flashcardDTOs.Add(new FlashcardDTO(flashcardId, front, back));
+                    }
+                }
+
+                foreach (FlashcardDTO flashcardDTO in flashcardDTOs)
+                {
+                    string selectionString = "";
+                    selectionString += flashcardDTO.FlashcardId;
+                    selectionString += $"\t{flashcardDTO.Front}";
+                    selectionString += $"\t{flashcardDTO.Back}";
+                    flashcardSelectionOptions.Add(selectionString);
+                }
+
+                flashcardSelectionOptions.Add("Return to Manage Stacks Menu");
+
+                Console.WriteLine("Choose a flashcard to update");
+                var selection = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                    .Title("-------------------------------")
+                    .PageSize(10)
+                    .AddChoices(flashcardSelectionOptions));
+
+                if (selection == "Return to Manage Stacks Menu")
+                {
+                    Console.Clear();
+                    ShowMenu();
+                }
+
+                Console.WriteLine("Enter new front of flashcard");
+                string updatedFront = Console.ReadLine();
+
+                Console.WriteLine("Enter new back of flashcard");
+                string updatedBack = Console.ReadLine();
+
+                // naively update both even if user enters same front and back...
+                string updateFlashcardQuery = $"UPDATE flashcards SET Front = @UpdatedFront, Back = @UpdatedBack WHERE FlashcardId = @FlashcardId";
+                using (SqlCommand updateFlashcardCommand = new SqlCommand(updateFlashcardQuery, connection))
+                {
+                    int flashcardId = Int32.Parse(selection.Split('\t')[0]);
+                    updateFlashcardCommand.Parameters.AddWithValue("@FlashcardId", flashcardId);
+                    updateFlashcardCommand.Parameters.AddWithValue("@UpdatedFront", updatedFront);
+                    updateFlashcardCommand.Parameters.AddWithValue("@UpdatedBack", updatedBack);
+                    int rowsAffected = updateFlashcardCommand.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine($"Flashcard {flashcardId} updated successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"No flashcard found with the name '{selection}'.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            DatabaseHelper.CloseConnection(connection);
         }
     }
 
@@ -197,7 +295,7 @@ internal class FlashcardsInterface
                     flashcardSelectionOptions.Add(selectionString);
                 }
 
-                flashcardSelectionOptions.Add("Return to Manage Stacks Menu");
+                flashcardSelectionOptions.Add("Return to Manage Flashcards Menu");
 
                 Console.WriteLine("Choose a flashcard to delete or return to main menu");
                 var selection = AnsiConsole.Prompt(
@@ -206,18 +304,18 @@ internal class FlashcardsInterface
                     .PageSize(10)
                     .AddChoices(flashcardSelectionOptions));
 
-                if (selection == "Return to Manage Stacks Menu")
+                if (selection == "Return to Manage Flashcards Menu")
                 {
                     Console.Clear();
                     ShowMenu();
                 }
 
-                string deleteStackQuery = $"DELETE FROM flashcards WHERE FlashcardId = @FlashcardId";
-                using (SqlCommand deleteStackCommand = new SqlCommand(deleteStackQuery, connection))
+                string deleteFlashcardQuery = $"DELETE FROM flashcards WHERE FlashcardId = @FlashcardId";
+                using (SqlCommand deleteFlashcardCommand = new SqlCommand(deleteFlashcardQuery, connection))
                 {
-                    deleteStackCommand.Parameters.AddWithValue("@FlashcardId", Int32.Parse(selection.Split('\t')[0]));
+                    deleteFlashcardCommand.Parameters.AddWithValue("@FlashcardId", Int32.Parse(selection.Split('\t')[0]));
 
-                    int rowsAffected = deleteStackCommand.ExecuteNonQuery();
+                    int rowsAffected = deleteFlashcardCommand.ExecuteNonQuery();
 
                     if (rowsAffected > 0)
                     {
