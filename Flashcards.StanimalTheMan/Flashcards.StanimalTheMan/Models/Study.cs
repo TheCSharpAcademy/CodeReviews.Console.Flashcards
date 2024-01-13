fa68e79235b10d140792f4cff4a0bbebdc482247
Stan Choi <sjc591@nyu.edu>
2024-01-13 13:05:29 -0500

    Make Study session model

---
+++ Flashcards.StanimalTheMan/Flashcards.StanimalTheMan/Models/Study.cs
@@ -0,0 +1,19 @@
+﻿namespace Flashcards.StanimalTheMan.Models;
+
+internal class Study
+{
+    public Study(int studySessionId, DateTime date, int score, int stackId)
+    {
+        StudySessionId = studySessionId;
+        Date = date;
+        Score = score;
+        StackId = stackId;
+    }
+
+    public int StudySessionId { get; set; }
+    public DateTime Date { get; set; }
+
+    public int Score { get; set; }
+
+    public int StackId { get; set; }
+}
