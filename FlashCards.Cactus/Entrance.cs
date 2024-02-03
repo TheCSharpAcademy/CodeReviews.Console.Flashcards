7e140a15838f82afc6421eeb71e1a0930950ff26
Jingwen Xia <amyfor7@gmail.com>
2024-02-03 19:11:48 +0800

    chore: init project

---
+++ FlashCards.Cactus/Entrance.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleTableExt;
+
+namespace FlashCards;
+public class Entrance
+{
+    public static void Main(string[] args)
+    {
+        bool endApp = false;
+        while (!endApp)
+        {
+            PrintMenu();
+            string? op = Console.ReadLine();
+            switch (op)
+            {
+                case "0":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
+            Console.Write("\nPress 'q' and Enter to close the app, or press any other key and Enter to return MAIN MENU. ");
+            if (Console.ReadLine() == "q") endApp = true;
+            Console.WriteLine("\n");
+        }
+    }
+
+    private static void PrintMenu()
+    {
+        Console.Clear();
+
+        List<string> menuData = new List<string>
+            {
+                "<0> Exit app.",
+                "<1> Manage Stacks.",
+                "<2> Manage FlashCards.",
+                "<3> Study.",
+                "<4> Study report."
+            };
+
+        ConsoleTableBuilder
+        .From(menuData)
+        .WithTitle("MAIN MENU", ConsoleColor.Yellow, ConsoleColor.DarkGray)
+        .WithFormat(ConsoleTableBuilderFormat.Alternative)
+        .ExportAndWriteLine(TableAligntment.Center);
+    }
+}
---
+++ FlashCards.Cactus/flashCardsSql.sql
@@ -0,0 +1,46 @@
+﻿USE flashcardsdb;
+
+-- Create a new table called 'Stack' in schema 'flashcardsdb.dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('flashcardsdb.dbo.Stack', 'U') IS NOT NULL
+DROP TABLE flashcardsdb.dbo.Stack
+GO
+-- Create the table in the specified schema
+CREATE TABLE flashcardsdb.dbo.Stack
+(
+    sid INT NOT NULL PRIMARY KEY IDENTITY(1, 1), -- primary key column
+    name [NVARCHAR](50) UNIQUE NOT NULL,
+);
+GO
+
+-- Create a new table called 'FlashCard' in schema 'flashcardsdb.dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('flashcardsdb.dbo.FlashCard', 'U') IS NOT NULL
+DROP TABLE flashcardsdb.dbo.FlashCard
+GO
+-- Create the table in the specified schema
+CREATE TABLE flashcardsdb.dbo.FlashCard
+(
+    fid INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    front VARCHAR(200),
+    back VARCHAR(200),
+    sid INT FOREIGN KEY REFERENCES Stack(sid)
+);
+GO
+
+-- Create a new table called 'StudySession' in schema 'flashcardsdb.dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('flashcardsdb.dbo.StudySession', 'U') IS NOT NULL
+DROP TABLE flashcardsdb.dbo.StudySession
+GO
+-- Create the table in the specified schema
+CREATE TABLE flashcardsdb.dbo.StudySession
+(
+    ssid INT NOT NULL PRIMARY KEY IDENTITY(1, 1) , -- primary key column
+    sid INT FOREIGN KEY REFERENCES Stack(sid),
+    date DATETIME,
+    score Int,
+);
+GO
+
+SELECT @@VERSION AS 'SQL Server Version';
