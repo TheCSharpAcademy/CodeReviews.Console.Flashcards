49fb75917b4214b3bb9849bf696413399de80761
Jingwen Xia <amyfor7@gmail.com>
2024-02-06 20:48:27 +0800

    feat: add data model

---
+++ FlashCards.Cactus/DataModel/StudySession.cs
@@ -0,0 +1,28 @@
+﻿namespace FlashCards.Cactus.DataModel
+{
+    public class StudySession
+    {
+        #region Constructors
+        public StudySession() { }
+
+        public StudySession(int id, int sId, DateTime date, int score)
+        {
+            Id = id;
+            SId = sId;
+            Date = date;
+            Score = score;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+        public int Id { get; set; }
+        public int SId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Score { get; set; }
+
+        #endregion Properties
+    }
+}
