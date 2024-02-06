e008a580670808af055a1fa3965876071286cb3a
Jingwen Xia <amyfor7@gmail.com>
2024-02-06 22:24:08 +0800

    refactor: extract common methods

--- FlashCards.Cactus/Entrance.cs
+++ FlashCards.Cactus/Entrance.cs
@@ -1,5 +1,4 @@
-﻿using ConsoleTableExt;
-using FlashCards.Cactus;
+﻿using FlashCards.Cactus;
 
 namespace FlashCards;
 public class Entrance
@@ -7,43 +6,6 @@ public class Entrance
     public static void Main(string[] args)
     {
         Application app = new Application();
-
-        while (true)
-        {
-            PrintMenu();
-            string? op = Console.ReadLine();
-            switch (op)
-            {
-                case "0":
-                    Environment.Exit(0);
-                    break;
-                case "1":
-                    app.ManageStacks();
-                    break;
-                default:
-                    break;
-            }
-            Console.WriteLine("\n");
-        }
-    }
-
-    private static void PrintMenu()
-    {
-        Console.Clear();
-
-        List<string> menuData = new List<string>
-            {
-                "<0> Exit app.",
-                "<1> Manage Stacks.",
-                "<2> Manage FlashCards.",
-                "<3> Study.",
-                "<4> Study report."
-            };
-
-        ConsoleTableBuilder
-        .From(menuData)
-        .WithTitle("MAIN MENU", ConsoleColor.Yellow, ConsoleColor.DarkGray)
-        .WithFormat(ConsoleTableBuilderFormat.Alternative)
-        .ExportAndWriteLine(TableAligntment.Center);
+        app.run();
     }
 }
---
+++ FlashCards.Cactus/MenuUtils.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleTableExt;
+
+namespace FlashCards.Cactus
+{
+    public class MenuUtils
+    {
+        public static void PrintMenu(string name, List<String> menu)
+        {
+            Console.Clear();
+
+            ConsoleTableBuilder
+            .From(menu)
+            .WithTitle(name, ConsoleColor.Yellow, ConsoleColor.DarkGray)
+            .WithFormat(ConsoleTableBuilderFormat.Alternative)
+            .ExportAndWriteLine(TableAligntment.Center);
+        }
+    }
+}
