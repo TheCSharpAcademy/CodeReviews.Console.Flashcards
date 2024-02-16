6871cbcc56b76078fc9e736a398d824ff043afa7
Jingwen Xia <amyfor7@gmail.com>
2024-02-16 14:52:14 +0800

    refactor: extract management classes

---
+++ FlashCards.Cactus/StackMangement.cs
@@ -0,0 +1,23 @@
+﻿using FlashCards.Cactus.DataModel;
+
+namespace FlashCards.Cactus;
+public class StackMangement
+{
+    public List<Stack> Stacks { get; set; }
+
+    public void ShowStacks()
+    {
+        Console.WriteLine("Show all stacks.");
+    }
+
+    public void AddStack()
+    {
+        Console.WriteLine("Add a new stack.");
+    }
+
+    public void DeleteStack()
+    {
+        Console.WriteLine("Delete a stack.");
+    }
+}
+
