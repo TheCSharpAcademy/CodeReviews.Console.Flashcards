206c10cbb7edebf6ca7a0a315fb12c1173a04db1
Jingwen Xia <amyfor7@gmail.com>
2024-02-17 17:10:06 +0800

    feat: mock Study Report

--- FlashCards.Cactus/Service/StudyReportService.cs
+++ FlashCards.Cactus/Service/StudyReportService.cs
@@ -1,10 +1,23 @@
-﻿namespace FlashCards.Cactus.Service;
+﻿using System.Globalization;
+using Spectre.Console;
+
+namespace FlashCards.Cactus.Service;
 
 public class StudyReportService
 {
     public void ShowStudyReportInSpecificYear()
     {
-        Console.WriteLine("Show  the study report from a specific year.");
+        Console.WriteLine("Input a year in format YYYY");
+        string? yearStr = Console.ReadLine();
+        DateTime parseTmp;
+        DateTime.TryParseExact(yearStr, "yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out parseTmp);
+        int year = parseTmp.Year;
+
+        var table = new Table();
+        table.Title($"Average score per month in {year}");
+        table.AddColumns("StackName", "Jan.", "Feb.", "Mar.", "Apr.", "May.", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec.");
+        table.AddRow("Word", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0");
+        AnsiConsole.Write(table);
     }
 }
 
