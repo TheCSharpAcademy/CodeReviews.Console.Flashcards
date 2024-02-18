7f108f0386e704ef67c6253beb79b4d68de53d10
Jingwen Xia <amyfor7@gmail.com>
2024-02-18 18:19:22 +0800

    feat: finish study report task

--- FlashCards.Cactus/Dao/StudySessionDao.cs
+++ FlashCards.Cactus/Dao/StudySessionDao.cs
@@ -1,5 +1,6 @@
 ﻿using FlashCards.Cactus.DataModel;
 using Microsoft.Data.SqlClient;
+using System.Data.SqlTypes;
 
 namespace FlashCards.Cactus.Dao;
 public class StudySessionDao
@@ -65,6 +66,44 @@ public class StudySessionDao
             rowsAffected = deleteCommand.ExecuteNonQuery();
         }
         return rowsAffected;
+    }
+
+    public List<Tuple<string, List<string>>> GetAverageScorePerMontInSpecYear(int year)
+    {
+        List<Tuple<string, List<string>>> results = new List<Tuple<string, List<string>>>();
+        using (var connection = new SqlConnection(DBConnectionStr))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = $@"SELECT * from 
+                (SELECT stackName, MONTH(date) month, score FROM StudySession
+                    WHERE YEAR(date) = {year}
+                ) as nt
+                PIVOT ( SUM(score) FOR month in 
+                ([1], [2], [3], [4], [5], [6], [7], [8], 
+                [9], [10], [11], [12]) ) as res; ";
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string stackName = reader.GetString(0);
+                    List<string> scores = new List<string>();
+                    for (int i = 1; i <= 12; i++)
+                    {
+                        try
+                        {
+                            scores.Add(reader.GetInt32(i).ToString());
+                        }
+                        catch (SqlNullValueException)
+                        {
+                            scores.Add("0");
+                        }
+                    }
+                    results.Add(new Tuple<string, List<string>>(stackName, scores));
+                }
+            }
+        }
+        return results;
     }
 }
 
