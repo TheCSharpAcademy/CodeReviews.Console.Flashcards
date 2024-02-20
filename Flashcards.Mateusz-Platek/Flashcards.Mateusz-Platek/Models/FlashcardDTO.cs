0231117a15abe742ba7bc09f45e1d14f3354699a
Mateusz-Platek <mplatek1705@gmail.com>
2024-02-20 22:43:34 +0100

    Project finished

---
+++ Flashcards.Mateusz-Platek/Flashcards.Mateusz-Platek/Models/FlashcardDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Flashcards.Mateusz_Platek.Models;
+
+public class FlashcardDTO
+{
+    public string word { get; }
+    public string translation { get; }
+
+    public FlashcardDTO(string word, string translation)
+    {
+        this.word = word;
+        this.translation = translation;
+    }
+}
