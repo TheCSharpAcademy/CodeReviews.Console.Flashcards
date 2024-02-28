a201a57155059c7d20bc5b433421ca487759195a
Young JIN <yjin@convoy-supply.com>
2024-02-27 16:10:28 -0800

    project finished

---
+++ Flash/Flash/Helper/DTO/FlashcardDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Flash;
+public class FlashcardDto
+{
+    public int Flashcard_Primary_Id { get; set; }
+    public string Front { get; set; }
+    public string Back { get; set; }
+    public int Stack_Primary_Id { get; set; }
+    
+}
