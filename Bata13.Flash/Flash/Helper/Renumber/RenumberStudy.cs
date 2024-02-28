5326bce3078e580e03e4327dd30654ba0ddb98db
Young JIN <yjin@convoy-supply.com>
2024-02-27 16:10:28 -0800

    project finished

---
+++ Bata13.Flash/Flash/Helper/Renumber/RenumberStudy.cs
@@ -0,0 +1,13 @@
+﻿using Flash.Helper.DTO;
+
+namespace Flash.Helper.Renumber;
+internal class RenumberStudy
+{
+    internal static void GetRenumberStudys(List<StudyDto> studys)
+    {
+        for (int i = 0; i < studys.Count; i++)
+        {
+            studys[i].Study_Primary_Id = i + 1;
+        }
+    }
+}
