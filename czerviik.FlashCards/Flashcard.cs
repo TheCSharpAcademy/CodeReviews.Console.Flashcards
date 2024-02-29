3edb66fa2815927084431be1459ca8148e1cdea0
Jiří Malík <czerviik@gmail.com>
2024-02-29 14:01:15 +0100

    change Flashcard DTOs

--- czerviik.FlashCards/Flashcard.cs
+++ czerviik.FlashCards/Flashcard.cs
@@ -16,12 +16,12 @@ public class Flashcard
 public class FlashcardQuestionDto
 {
     public string Question { get; set; }
-    public int StackId { get; set; }
 }
 
 public class FlashcardReviewDto
 {
+    public int DisplayId { get; set; }
     public string Question { get; set; }
-    public string Answer {get; set;}
-    public int StackId { get; set;}
+    public string Answer { get; set; }
 }
+//prostudovat DTOS v discordu (při nových indexech vytvořit nový list DTO a zpětné mapování bude probíhat na základě pořadí ID v daném listu. (hledej v discordu "Let's say you have 3 items in the db with indexes 10, 20 and 30" ))
