ca52f4c43a9e1ee120d42b444274106dbe976b2d
Samuel Gannon <sam.gannon@sportssouth.biz>
2024-03-01 13:41:25 -0600

    // removed unused using statements

--- Flashcards.SamGannon/DataAccess/Models/Flashcard.cs
+++ Flashcards.SamGannon/DataAccess/Models/Flashcard.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DataAccess.Models;
 
-namespace DataAccess.Models
+public class Flashcard
 {
-    public class Flashcard
-    {
-        public int FlashcardId {  get; set; }
-        public int StackId { get; set; }
-        public string? Question { get; set; } = string.Empty;
-        public string? Answer { get; set; } = string.Empty;
-    }
+    public int FlashcardId {  get; set; }
+    public int StackId { get; set; }
+    public string? Question { get; set; } = string.Empty;
+    public string? Answer { get; set; } = string.Empty;
 }
