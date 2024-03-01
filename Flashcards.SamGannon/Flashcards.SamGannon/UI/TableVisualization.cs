799a0f82ded2e5193ef93581b9cd88b4f4fdcfaf
Samuel Gannon <sam.gannon@sportssouth.biz>
2024-03-01 13:57:07 -0600

    // removed unused using statements

--- Flashcards.SamGannon/Flashcards.SamGannon/UI/TableVisualization.cs
+++ Flashcards.SamGannon/Flashcards.SamGannon/UI/TableVisualization.cs
@@ -1,39 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ConsoleTableExt;
+﻿using ConsoleTableExt;
 using DataAccess.Models;
 
-namespace Flashcards.SamGannon.UI
+namespace Flashcards.SamGannon.UI;
+
+internal class TableVisualization
 {
-    internal class TableVisualization
+    internal static void ShowTable<T>(List<T> tableData, string tableTitle) where T : class
     {
-        internal static void ShowTable<T>(List<T> tableData, string tableTitle) where T : class
-        {
-            Console.Clear();
-            Console.WriteLine("\n\n");
+        Console.Clear();
+        Console.WriteLine("\n\n");
 
-            ConsoleTableBuilder
-                .From(tableData)
-                .WithTitle(tableTitle)
-                .ExportAndWriteLine();
-            Console.Write("\n\n");
-            
-        }
+        ConsoleTableBuilder
+            .From(tableData)
+            .WithTitle(tableTitle)
+            .ExportAndWriteLine();
+        Console.Write("\n\n");
+        
+    }
 
-        internal static void ShowSingleRow<T>(List<T> tableData, string tableTitle) where T : class
-        {
-            Console.Clear();
-            Console.WriteLine("\n\n");
+    internal static void ShowSingleRow<T>(List<T> tableData, string tableTitle) where T : class
+    {
+        Console.Clear();
+        Console.WriteLine("\n\n");
 
-            ConsoleTableBuilder
-                .From(tableData)
-                .WithTitle(tableTitle)
-                .ExportAndWriteLine();
-            Console.Write("\n\n");
-            
-        }
+        ConsoleTableBuilder
+            .From(tableData)
+            .WithTitle(tableTitle)
+            .ExportAndWriteLine();
+        Console.Write("\n\n");
+        
     }
 }
