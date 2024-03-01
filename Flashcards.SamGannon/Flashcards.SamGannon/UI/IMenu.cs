67f5a6f1c24d1db2fe49f46b0fa4bd3af75c268b
Samuel Gannon <sam.gannon@sportssouth.biz>
2024-03-01 13:50:05 -0600

    // removed unused using statements

--- Flashcards.SamGannon/Flashcards.SamGannon/UI/IMenu.cs
+++ Flashcards.SamGannon/Flashcards.SamGannon/UI/IMenu.cs
@@ -1,15 +1,9 @@
 ﻿using DataAccess;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Flashcards.SamGannon.UI
+namespace Flashcards.SamGannon.UI;
+
+public interface IMenu
 {
-    public interface IMenu
-    {
-        IDataAccess DataAccess { get; }
-        void ShowMenu();
-    }
+    IDataAccess DataAccess { get; }
+    void ShowMenu();
 }
