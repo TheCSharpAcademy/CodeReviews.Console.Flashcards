75077287bbadbd038fdb1a1f248357bba651b117
Samuel Gannon <sam.gannon@sportssouth.biz>
2024-03-01 13:42:55 -0600

    // removed unused using statements

--- Flashcards.SamGannon/DataAccess/Models/StudyHistory.cs
+++ Flashcards.SamGannon/DataAccess/Models/StudyHistory.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DataAccess.Models;
 
-namespace DataAccess.Models
+public class StudyHistory
 {
-    public class StudyHistory
-    {
-        public int Id { get; set; }
-        public int StackId { get; set; }
-        public string? StackName { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
-        public decimal Score { get; set; }
-    }
+    public int Id { get; set; }
+    public int StackId { get; set; }
+    public string? StackName { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public decimal Score { get; set; }
 }
