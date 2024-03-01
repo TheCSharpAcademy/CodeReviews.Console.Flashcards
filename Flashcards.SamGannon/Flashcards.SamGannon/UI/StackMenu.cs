ce18f4881e394bad83e249aa44b4823f65f7a5d1
Samuel Gannon <sam.gannon@sportssouth.biz>
2024-03-01 13:52:40 -0600

    // removed unused using statements

--- Flashcards.SamGannon/Flashcards.SamGannon/UI/StackMenu.cs
+++ Flashcards.SamGannon/Flashcards.SamGannon/UI/StackMenu.cs
@@ -2,203 +2,197 @@
 using DataAccess.Models;
 using Flashcards.SamGannon.DTOs;
 using Flashcards.SamGannon.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Flashcards.SamGannon.UI
-{
-    public class StackMenu : IMenu
-    { 
-        private readonly IMenu _MainMenu;
+namespace Flashcards.SamGannon.UI;
 
-        public StackMenu(MainMenu mainMenu)
+public class StackMenu : IMenu
+{ 
+    private readonly IMenu _MainMenu;
+
+    public StackMenu(MainMenu mainMenu)
+    {
+        _MainMenu = mainMenu;
+    }
+
+    public IDataAccess DataAccess => _MainMenu.DataAccess;
+
+    public void ShowMenu()
+    {
+        MenuMessages.ShowStackMenu();
+        string choice = Console.ReadLine();
+        StackMenuOptions(choice);
+    }
+
+    private void StackMenuOptions(string? choice)
+    {
+        while (choice != "1" && choice != "2" && choice != "3" && choice != "4")
         {
-            _MainMenu = mainMenu;
+            Console.WriteLine("Invalid choice. Please enter 1, 2, 3, or 4.");
+            choice = Console.ReadLine();
         }
 
-        public IDataAccess DataAccess => _MainMenu.DataAccess;
-
-        public void ShowMenu()
+        switch (choice)
         {
-            MenuMessages.ShowStackMenu();
-            string choice = Console.ReadLine();
-            StackMenuOptions(choice);
-        }
-
-        private void StackMenuOptions(string? choice)
-        {
-            while (choice != "1" && choice != "2" && choice != "3" && choice != "4")
-            {
+            case "1":
+                CreateStack();
+                break;
+            case "2":
+                EditStacks();
+                break;
+            case "3":
+                DeleteStacks();
+                break;
+            case "4":
+                ShowMenu();
+                break;
+            default:
                 Console.WriteLine("Invalid choice. Please enter 1, 2, 3, or 4.");
-                choice = Console.ReadLine();
-            };
+                break;
+        }
+    }
 
-            switch (choice)
+    private void CreateStack()
+    {
+        string stackName = InputStackName();
+        DataAccess.AddStack(stackName);
+
+        Console.WriteLine($"Stack '{stackName}' created successfully! Press a key to go back to the Main Menu.");
+        Console.Read();
+        _MainMenu.ShowMenu();
+    }
+
+    private string InputStackName()
+    {
+        Console.Clear();
+        Console.WriteLine("Enter the name for your new stack:");
+        Console.WriteLine();
+        Console.WriteLine("**Please note: Stack names are not case sensitive.**");
+        Console.WriteLine("IE. Spanish, spanish, SPANISH, SpAnISh are all the same input.");
+        string? stackName = Console.ReadLine();
+        string formattedStackName = stackName.Trim().ToUpper();
+        int iAttempts = 0;
+
+        while (string.IsNullOrEmpty(stackName))
+        {
+            if (iAttempts < 3)
             {
-                case "1":
-                    CreateStack();
-                    break;
-                case "2":
-                    EditStacks();
-                    break;
-                case "3":
-                    DeleteStacks();
-                    break;
-                case "4":
-                    ShowMenu();
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice. Please enter 1, 2, 3, or 4.");
-                    break;
+                Console.WriteLine("Please enter a name for your new flashcard stack");
+                stackName = Console.ReadLine();
+                formattedStackName = stackName.Trim().ToUpper();
+                iAttempts++;
+            }
+            else
+            {
+                Console.WriteLine("You have exceeded the amount of attempts.");
+                Environment.Exit(1);
             }
         }
 
-        private void CreateStack()
-        {
-            string stackName = InputStackName();
-            DataAccess.AddStack(stackName);
+        iAttempts = 0;
+        bool stackExists = DataAccess.CheckStackName(formattedStackName);
 
-            Console.WriteLine($"Stack '{stackName}' created successfully! Press a key to go back to the Main Menu.");
-            Console.Read();
+        while (stackExists)
+        {
+            if (iAttempts < 3)
+            {
+                Console.WriteLine($"A stack with the name '{stackName}' already exists. Please enter a different name");
+                stackName = Console.ReadLine();
+                formattedStackName = stackName.Trim().ToUpper();
+                stackExists = DataAccess.CheckStackName(formattedStackName);
+                iAttempts++;
+            }
+            else
+            {
+                Console.WriteLine("You have exceeded the amount of attempts.");
+                Environment.Exit(1);
+            }
+
+        }
+
+        iAttempts = 0;
+        return formattedStackName;
+    }
+
+    private void EditStacks()
+    {
+        ProcessStackOperation("edit");
+    }
+
+    private void DeleteStacks()
+    {
+        ProcessStackOperation("delete");
+    }
+
+    private void ProcessStackOperation(string editType)
+    {
+        ConsoleHelper.Map(DataAccess, "Stack Menu");
+
+        Console.WriteLine($"\n\nPlease enter the name of the stack you wish to {editType}:");
+        string stackName = ConsoleHelper.ValidateStackName(DataAccess);
+
+        if(stackName == null )
+        {
             _MainMenu.ShowMenu();
         }
 
-        private string InputStackName()
-        {
-            Console.Clear();
-            Console.WriteLine("Enter the name for your new stack:");
-            Console.WriteLine();
-            Console.WriteLine("**Please note: Stack names are not case sensitive.**");
-            Console.WriteLine("IE. Spanish, spanish, SPANISH, SpAnISh are all the same input.");
-            string? stackName = Console.ReadLine();
-            string formattedStackName = stackName.Trim().ToUpper();
-            int iAttempts = 0;
+        StackModel rawSingleStack = DataAccess.GetStackByName(stackName);
+        StackDto singleStack = new StackDto(rawSingleStack);
 
-            while (string.IsNullOrEmpty(stackName))
+        EditStacksSubMenu(singleStack, editType);
+    }
+
+    private void EditStacksSubMenu(StackDto stack, string editType)
+    {
+        TableVisualization.ShowSingleRow(new List<StackDto> { stack }, stack.StackName);
+
+        Console.WriteLine($"\nIs this the stack you wish to {editType}? (Y/N)");
+        Console.WriteLine($"Y - {editType} stack");
+        Console.WriteLine("N - back to Stack list\n");
+
+        string formattedChoice = ConsoleHelper.GetValidChoice();
+
+        MakeEditSelection(stack, formattedChoice, editType);
+    }
+
+    private void MakeEditSelection(StackDto stack, string formattedChoice, string editType)
+    {
+        if (formattedChoice == "Y")
+        {
+            if (editType == "edit")
             {
-                if (iAttempts < 3)
-                {
-                    Console.WriteLine("Please enter a name for your new flashcard stack");
-                    stackName = Console.ReadLine();
-                    formattedStackName = stackName.Trim().ToUpper();
-                    iAttempts++;
-                }
-                else
-                {
-                    Console.WriteLine("You have exceeded the amount of attempts.");
-                    Environment.Exit(1);
-                }
+                EditStackName(stack);
             }
-
-            iAttempts = 0;
-            bool stackExists = DataAccess.CheckStackName(formattedStackName);
-
-            while (stackExists)
+            else if (editType == "delete")
             {
-                if (iAttempts < 3)
-                {
-                    Console.WriteLine($"A stack with the name '{stackName}' already exists. Please enter a different name");
-                    stackName = Console.ReadLine();
-                    formattedStackName = stackName.Trim().ToUpper();
-                    stackExists = DataAccess.CheckStackName(formattedStackName);
-                    iAttempts++;
-                }
-                else
-                {
-                    Console.WriteLine("You have exceeded the amount of attempts.");
-                    Environment.Exit(1);
-                }
-
-            }
-
-            iAttempts = 0;
-            return formattedStackName;
-        }
-
-        private void EditStacks()
-        {
-            ProcessStackOperation("edit");
-        }
-
-        private void DeleteStacks()
-        {
-            ProcessStackOperation("delete");
-        }
-
-        private void ProcessStackOperation(string editType)
-        {
-            ConsoleHelper.Map(DataAccess, "Stack Menu");
-
-            Console.WriteLine($"\n\nPlease enter the name of the stack you wish to {editType}:");
-            string stackName = ConsoleHelper.ValidateStackName(DataAccess);
-
-            if(stackName == null )
-            {
-                _MainMenu.ShowMenu();
-            }
-
-            StackModel rawSingleStack = DataAccess.GetStackByName(stackName);
-            StackDto singleStack = new StackDto(rawSingleStack);
-
-            EditStacksSubMenu(singleStack, editType);
-        }
-
-        private void EditStacksSubMenu(StackDto stack, string editType)
-        {
-            TableVisualization.ShowSingleRow(new List<StackDto> { stack }, stack.StackName);
-
-            Console.WriteLine($"\nIs this the stack you wish to {editType}? (Y/N)");
-            Console.WriteLine($"Y - {editType} stack");
-            Console.WriteLine("N - back to Stack list\n");
-
-            string formattedChoice = ConsoleHelper.GetValidChoice();
-
-            MakeEditSelection(stack, formattedChoice, editType);
-        }
-
-        private void MakeEditSelection(StackDto stack, string formattedChoice, string editType)
-        {
-            if (formattedChoice == "Y")
-            {
-                if (editType == "edit")
-                {
-                    EditStackName(stack);
-                }
-                else if (editType == "delete")
-                {
-                    DeleteStack(stack);
-                }
-                else
-                {
-                    // handle error
-                }
-            }
-            else if (formattedChoice == "N")
-            {
-                EditStacksSubMenu(stack, editType);
+                DeleteStack(stack);
             }
             else
             {
                 // handle error
             }
         }
-
-        private void EditStackName(StackDto stack)
+        else if (formattedChoice == "N")
         {
-            string newStackName = InputStackName();
-            DataAccess.EditStackname(stack.StackName, newStackName);
-            Console.WriteLine("Stack name edited successfully! Press a key to continue.");
-            Console.ReadLine();
+            EditStacksSubMenu(stack, editType);
         }
-
-        private void DeleteStack(StackDto stack)
+        else
         {
-            DataAccess.DeleteStack(stack.StackName);
-            Console.WriteLine("Stack name deleted successfully! Press a key to continue.");
-            Console.ReadLine();
+            // handle error
         }
+    }
+
+    private void EditStackName(StackDto stack)
+    {
+        string newStackName = InputStackName();
+        DataAccess.EditStackname(stack.StackName, newStackName);
+        Console.WriteLine("Stack name edited successfully! Press a key to continue.");
+        Console.ReadLine();
+    }
+
+    private void DeleteStack(StackDto stack)
+    {
+        DataAccess.DeleteStack(stack.StackName);
+        Console.WriteLine("Stack name deleted successfully! Press a key to continue.");
+        Console.ReadLine();
     }
 }
