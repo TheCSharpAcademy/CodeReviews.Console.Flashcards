5a46311fe670ce1791da231e350210378e8a2040
Samuel Gannon <sam.gannon@sportssouth.biz>
2024-03-01 13:42:31 -0600

    // removed unused using statements

--- Flashcards.SamGannon/DataAccess/Models/StackModel.cs
+++ Flashcards.SamGannon/DataAccess/Models/StackModel.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DataAccess.Models;
 
-namespace DataAccess.Models
+public class StackModel
 {
-    public class StackModel
-    {
-        public int StackId { get; set; }
-        public string? StackName { get; set; } = string.Empty;
+    public int StackId { get; set; }
+    public string? StackName { get; set; } = string.Empty;
 
-    }
 }
