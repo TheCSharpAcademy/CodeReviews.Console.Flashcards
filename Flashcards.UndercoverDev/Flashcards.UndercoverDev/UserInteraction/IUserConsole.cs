faddd0999d38d350d686a6a115774d1faac74606
N-Endy <okafornel@gmail.com>
2024-06-03 22:00:03 +0100

    Add user interaction for main menu

---
+++ Flashcards.UndercoverDev/Flashcards.UndercoverDev/UserInteraction/IUserConsole.cs
@@ -0,0 +1,7 @@
+namespace Flashcards.UndercoverDev.UserInteraction
+{
+    public interface IUserConsole
+    {
+        void MainMenu();
+    }
+}
---
+++ Flashcards.UndercoverDev/Flashcards.UndercoverDev/UserInteraction/UserConsole.cs
@@ -0,0 +1,28 @@
+using Spectre.Console;
+
+namespace Flashcards.UndercoverDev.UserInteraction
+{
+    public class UserConsole : IUserConsole
+    {
+        public void MainMenu()
+        {
+            var menu = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Welcome. Select a [blue]function[/]?")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                    .AddChoices([
+                        "Add a Stack",
+                        "Delete a Stack",
+                        "Add a Flashcard to a Stack",
+                        "Delete a Flashcard from a Stack",
+                        "Study Session",
+                        "View Study Session by Stack",
+                        "Average Score Yearly Report",
+                        "Exit"
+                        ]
+                    )
+            );
+        }
+    }
+}
