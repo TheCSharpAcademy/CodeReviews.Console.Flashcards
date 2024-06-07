7772ac65f0b894b470cb0d5abf979b22991188ca
N-Endy <okafornel@gmail.com>
2024-06-07 18:56:47 +0100

    Create ToFlashcardDTO method

--- Flashcards.UndercoverDev/Flashcards.UndercoverDev/Models/Flashcard.cs
+++ Flashcards.UndercoverDev/Flashcards.UndercoverDev/Models/Flashcard.cs
@@ -6,18 +6,8 @@ namespace Flashcards.UndercoverDev.Models
         public int StackId { get; set; }
         public string Question { get; set; } = "";
         public string Answer { get; set; } = "";
-    }
 
-    public class FlashcardDTO
-    {
-        public int StackId { get; set; }
-        public string Question { get; set; } = "";
-        public string Answer { get; set; } = "";
-    }
-
-    public class FlashCardMapper
-    {
-        public static FlashcardDTO Map(Flashcard flashcard)
+        public static FlashcardDTO ToFlashcardDTO(Flashcard flashcard)
         {
             return new FlashcardDTO
             {
@@ -25,5 +15,12 @@ namespace Flashcards.UndercoverDev.Models
                 Answer = flashcard.Answer
             };
         }
+    }
+
+    public class FlashcardDTO
+    {
+        public int StackId { get; set; }
+        public string Question { get; set; } = "";
+        public string Answer { get; set; } = "";
     }
 }
