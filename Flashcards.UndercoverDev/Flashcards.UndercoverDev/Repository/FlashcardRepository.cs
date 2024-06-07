fdc5ac136d12c6291f56ddd3ea1279dc5a175d88
N-Endy <okafornel@gmail.com>
2024-06-08 00:11:20 +0100

    Add post method for flashcard

--- Flashcards.UndercoverDev/Flashcards.UndercoverDev/Repository/FlashcardRepository.cs
+++ Flashcards.UndercoverDev/Flashcards.UndercoverDev/Repository/FlashcardRepository.cs
@@ -1,7 +1,38 @@
+
+using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+using Flashcards.UndercoverDev.Models;
+
 namespace Flashcards.UndercoverDev.Repository
 {
-    public class FlashcardRepository
+    public class FlashcardRepository : IFlashcardRepository
     {
-        
+        const string _databaseName = "FlashcardDB";
+        private readonly string _connectionStringTemplate = ConfigurationManager.AppSettings.Get("ConnectionString") ?? "";
+        private readonly string _connectionString;
+
+        public FlashcardRepository()
+        {
+            _connectionString = string.Format(_connectionStringTemplate, _databaseName);
+        }
+
+        public void Post(FlashcardDTO newFlashcard)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            string createFlashcardQuery = $"INSERT INTO Flashcards(StackId, Question, Answer) VALUES('{newFlashcard.StackId}', '{newFlashcard.Question}', '{newFlashcard.Answer}');";
+            connection.Execute(createFlashcardQuery);
+        }
+
+        public List<Flashcard> GetFlashcards()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            string getFlashcardsQuery = $"SELECT * FROM Flashcards;";
+
+            return connection.Query<Flashcard>(getFlashcardsQuery).ToList();
+        }
     }
 }
--- Flashcards.UndercoverDev/Flashcards.UndercoverDev/Repository/IFlashcardRepository.cs
+++ Flashcards.UndercoverDev/Flashcards.UndercoverDev/Repository/IFlashcardRepository.cs
@@ -1,7 +1,11 @@
+
+using Flashcards.UndercoverDev.Models;
+
 namespace Flashcards.UndercoverDev.Repository
 {
     public interface IFlashcardRepository
     {
-        
+        List<Flashcard> GetFlashcards();
+        void Post(FlashcardDTO newFlashcard);
     }
 }
