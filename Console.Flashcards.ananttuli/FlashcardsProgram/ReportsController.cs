8fe1eb698326568aa3649bfdddc960f848a02924
Anant Tuli <ananttuli.dev@gmail.com>
2024-06-25 02:41:37 +1000

    Refactor: extract code into method

--- Console.Flashcards.ananttuli/FlashcardsProgram/ReportsController.cs
+++ Console.Flashcards.ananttuli/FlashcardsProgram/ReportsController.cs
@@ -9,6 +9,9 @@ public class ReportsController(ReportsRepository reportsRepository)
 {
     public ReportsRepository reportsRepo = reportsRepository;
 
+    public static string[] MonthColumns = DateTimeFormatInfo.CurrentInfo.MonthNames
+        .Where(name => name.Trim().Length > 0).ToArray();
+
     public void AverageScorePerMonthPerStack()
     {
         AnsiConsole.MarkupLine("Average score per month per stack");
@@ -22,35 +25,50 @@ public class ReportsController(ReportsRepository reportsRepository)
             return;
         }
 
-        var monthColumns = DateTimeFormatInfo.CurrentInfo.MonthNames.Where(name => name.Trim().Length > 0);
-
         var table = new Table();
 
-        table.AddColumns(["Name", .. monthColumns]);
+        table.AddColumns(["Name", .. MonthColumns]);
 
         foreach (var result in scoresByStackByMonth)
         {
-            try
+            var (success, row) = GetRowFromResult(result);
+            if (success)
             {
-                var stackName = result?["Name"]?.ToString() ?? "-";
-                var monthAverages = monthColumns.Select(col =>
-                    {
-                        var average = result?[col] ?? null;
-                        return average == null ? "-" : $"{average}%";
-                    });
-
-                table.AddRow([
-                    stackName,
-                    ..monthAverages
-                ]);
+                table.AddRow(row);
             }
-            catch
+            else
             {
                 table.AddEmptyRow();
-                continue;
             }
         }
 
         AnsiConsole.Write(table);
+    }
+
+    private Tuple<bool, string[]> GetRowFromResult(Dictionary<string, object?>? result)
+    {
+        try
+        {
+            string stackName = result?["Name"]?.ToString() ?? "-";
+            IEnumerable<string> monthAverages = MonthColumns.Select(col =>
+                {
+                    var average = result?[col]?.ToString() ?? "";
+                    bool parseSuccess = decimal.TryParse(
+                        average, out decimal parsedValue
+                    );
+
+                    return parseSuccess ? parsedValue.ToString("0.0") :
+                        "-";
+                });
+
+            return new Tuple<bool, string[]>(true, [
+                stackName,
+                ..monthAverages
+            ]);
+        }
+        catch
+        {
+            return new Tuple<bool, string[]>(false, []);
+        }
     }
 }
