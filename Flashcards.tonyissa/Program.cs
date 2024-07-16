80d0ebe3d982d2393c78bcbcc49115776e206370
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-07-15 22:11:36 -0400

    Add basic program loop and displaying of main view

--- Flashcards.tonyissa/Program.cs
+++ Flashcards.tonyissa/Program.cs
@@ -1,3 +1,18 @@
 ﻿global using Microsoft.Data.SqlClient;
 global using Spectre.Console;
 global using Dapper;
+using Flashcards.MainView;
+
+while (true)
+{
+    try
+    {
+        var quit = MainViewController.InitMainView();
+        if (quit == true) return;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+        Console.ReadKey();
+    }
+}
--- Flashcards.tonyissa/Views/MainView.cs
+++ Flashcards.tonyissa/Views/MainView.cs
@@ -1,1 +1,32 @@
-﻿
+﻿namespace Flashcards.MainView;
+
+public static class MainViewController
+{
+    public static bool? InitMainView()
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to my flashcard app!");
+        Console.WriteLine("-Press s to view stacks");
+        Console.WriteLine("-Press f to view flashcards");
+        Console.WriteLine("-Press b to begin studying");
+        Console.WriteLine("-Press q to quit");
+
+        var option = GetMainViewInput();
+        Console.WriteLine("\nYou chose: " + option);
+        Console.ReadKey();
+
+        return null;
+    }
+
+    public static char GetMainViewInput()
+    {
+        var key = Console.ReadKey(true).KeyChar;
+
+        if (key != 's' && key != 'f' && key != 'b' && key != 'q')
+        {
+            return GetMainViewInput();
+        }
+
+        return key;
+    }
+}
