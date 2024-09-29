ac9c8b90a5491a9b9bf4fc78af19c7913e727c5c
empty-codes <morayotimothy@gmail.com>
2024-09-29 19:48:27 +0100

    -Create Database clas for database creation logic

---
+++ Flashcards.empty-codes/Data/Database.cs
@@ -0,0 +1,64 @@
+﻿using System.Data.SqlClient;
+using Spectre.Console;
+
+namespace Flashcards.empty_codes.Data;
+
+internal class Database
+{
+    private readonly string connectionString;
+
+    public Database(string connString)
+    {
+        connectionString = connString;
+    }
+
+    public void InitializeDatabase()
+    {
+        using var conn = new SqlConnection(connectionString);
+        conn.Open();
+
+        string createStacksTable = @"
+            CREATE TABLE IF NOT EXISTS Stacks (
+                StackId INT PRIMARY KEY IDENTITY(1,1),
+                StackName VARCHAR(255) NOT NULL UNIQUE
+            )";
+
+        string createFlashcardsTable = @"
+            CREATE TABLE IF NOT EXISTS Flashcards (
+                FlashcardId INT PRIMARY KEY IDENTITY(1,1),
+                Question VARCHAR(255),
+                Answer VARCHAR(255),
+                StackId INT,
+                FOREIGN KEY (StackId) REFERENCES Stacks(StackId) ON DELETE CASCADE
+            )";
+
+        string createStudySessionsTable = @"
+            CREATE TABLE IF NOT EXISTS StudySessions (
+                SessionId INT PRIMARY KEY IDENTITY(1,1),
+                StudyDate DATETIME,
+                Score INT,
+                StackId INT,
+                FOREIGN KEY (StackId) REFERENCES Stacks(StackId) ON DELETE CASCADE
+            )";
+
+        try
+        {
+            using var stacksTableCommand = new SqlCommand(createStacksTable, conn);
+            stacksTableCommand.ExecuteNonQuery();
+
+            using var flashcardsTableCommand = new SqlCommand(createFlashcardsTable, conn);
+            flashcardsTableCommand.ExecuteNonQuery();
+
+            using var studysessionsTableCommand = new SqlCommand(createStudySessionsTable, conn);
+            studysessionsTableCommand.ExecuteNonQuery();
+        }
+        catch (SqlException e)
+        {
+            {
+                AnsiConsole.MarkupLine($"[red]Error occurred while trying to create the database Table\n - Details: {e.Message}[/]");
+            }
+            AnsiConsole.MarkupLine($"[green]Database file {dbPath} successfully created.[/] [green]The database is ready to use.[/]");
+            Console.Clear();
+        }
+    }
+}
