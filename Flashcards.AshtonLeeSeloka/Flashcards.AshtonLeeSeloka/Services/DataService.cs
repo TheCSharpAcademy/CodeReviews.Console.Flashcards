29311f3e1dba00f7b30ccc5b39dd8b3ffd8aae6a
ashtonseloka@outlook.com <ashtonseloka@outlook.com>
2025-01-05 22:30:02 +0200

    Creates DB FlashCards if it does not exist.

--- Flashcards.AshtonLeeSeloka/Flashcards.AshtonLeeSeloka/Services/DataService.cs
+++ Flashcards.AshtonLeeSeloka/Flashcards.AshtonLeeSeloka/Services/DataService.cs
@@ -4,16 +4,23 @@ using System.Configuration;
 
 internal class DataService
 {
-	private readonly string? _connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
-	
+	private readonly string? _ConnectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+	private readonly string? _DBCreationString = ConfigurationManager.AppSettings.Get("DBCreationString");
 
 	public void createDB()
 	{
 		try
 		{
-			using (SqlConnection connection = new SqlConnection(_connectionString))
+			using (SqlConnection connection = new SqlConnection(_DBCreationString))
 			{
 				connection.Open();
+				
+				var SQLCommand = connection.CreateCommand();
+				SQLCommand.CommandText = @"IF NOT EXISTS (SELECT * FROM sys.databases WHERE name ='FlashCardsDB')
+															BEGIN
+																CREATE DATABASE FlashCardsDB;
+															END;";
+				SQLCommand.ExecuteNonQuery();
 				Console.WriteLine("Sucess)");
 			}
 		}
