253a32c0a6f196a6d326c708c4f1bb7a3ad45dcd
alexgit55 <121723219+alexgit55@users.noreply.github.com>
2025-01-06 16:15:10 -0500

    Initial database connection setup

--- Flashcards.alexgit55/Program.cs
+++ Flashcards.alexgit55/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var DataAccess = new DataAccess();
+            DataAccess.CreateTables();
+
+            Console.ReadKey();
         }
     }
 }
