9b73449c3d75ed0470c91ed4d919bd724dc831e3
devjunaeid <junaeidthamid@gmail.com>
2025-07-09 15:55:44 +0600

    Added: FlashCard DTO, Model, Controller.

---
+++ DTO/CardViewDto.cs
@@ -0,0 +1,14 @@
+namespace DotNETConsole.Flashcards.DTO;
+
+
+public class CardViewDto
+{
+    public int ID { get; }
+    public string Question { get; }
+    public string Stack { get; }
+
+    public override string ToString()
+    {
+        return $"{Question} - {Stack}";
+    }
+}
